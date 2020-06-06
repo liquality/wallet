6c65132ebb90f2e4ad89af195d498af15b0877a1
gr0kchain <gr0kchain@bitcoindev.network>
2020-06-06 18:55:37 +0200

    Added 24 word mnemonic option and PasteHop for supporting pasting 12 or 24 words

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import ToggleButton from 'vue-js-toggle-button'
 
 import '@/assets/scss/style.scss'
 
+Vue.use(ToggleButton)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/v2/ImportWallet.vue
+++ src/views/v2/ImportWallet.vue
@@ -5,11 +5,12 @@
     </div>
     <div class="import-wallet_top">
       <h2>Import wallet</h2>
-      <p>Enter the 12 word seed phrase saved when creating your wallet in the same order.</p>
+      <p>Enter the {{numWords}} word seed phrase saved when creating your wallet in the same order.</p>
     </div>
     <div class="import-wallet_bottom">
+      <toggle-button @change="toggleMnemonicLength" color="#9d4dfa"/> {{numWords}} words
       <form class="form import-wallet_seed">
-        <div v-for="(e, n) in 12" :key="n"><input type="text" class="form-control form-control-sm" v-model="wordList[n]" autocomplete="off" required /></div>
+        <div v-for="(e, n) in numWords" :key="n"><input type="text" class="form-control form-control-sm" v-model="wordList[n]" autocomplete="off" required /></div>
       </form>
       <p><button class="btn btn-primary btn-lg btn-block" :disabled="disableNext" @click="next">Continue</button></p>
       <p><button class="btn btn-light btn-outline-primary btn-lg btn-block btn-icon" @click="$router.go(-1)">Cancel</button></p>
@@ -27,7 +28,21 @@ export default {
   },
   data: function () {
     return {
-      wordList: Array(12).fill('')
+      wordList: Array(12).fill(''),
+      numWords : 12
+    }
+  },
+  updated: function() {
+  },
+  watch: {
+    wordList: function (newList, oldList) {
+      var words = newList[0].split(" ")
+      if (words.length == this.numWords) {
+        for ( var  m = 0; m < words.length; m++) {
+          this.wordList[m] = words[m]
+        }
+      }
+      
     }
   },
   computed: {
@@ -36,25 +51,34 @@ export default {
       return this.wallets.find(wallet => wallet.id === this.activeWalletId)
     },
     disableNext: function () {
-      return this.wordList.filter(word => word === '').length > 0 // TODO: this should actually validate bip39
+      return this.wordList.filter(word => word === '' || /\s/.test(word)).length > 0 // TODO: this should actually validate bip39
     }
   },
   methods: {
     next () {
       const mnemonic = this.wordList.join(' ')
       this.$router.push({ name: 'OnboardingPassword', params: { mnemonic } })
+    },
+    toggleMnemonicLength () {
+      this.numWords = (this.numWords == 12)? 24: 12
+      this.wordList = Array(this.numWords).fill('')
     }
   }
 }
 </script>
 
 <style lang="scss">
+
+.num-words {
+  background: $color-text-primary;
+
+}
+
 .import-wallet {
   padding: 50px 0 0 0 !important;
+  overflow-y: scroll;
 
   .import-wallet_top {
-    flex-grow: 1;
-
     h2 {
       padding-top: 10px;
       margin-bottom: 10px;
@@ -62,10 +86,10 @@ export default {
   }
 
   .import-wallet_bottom {
-    flex: 1;
     background: #FFFFFF;
     color: $color-text-primary;
     padding: $wrapper-padding;
+    
   }
 
   > div {
