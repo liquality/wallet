f819e1f1fb47f9ff460c70d4b4191f90ffbff847
monokh <mnokhb@gmail.com>
2020-07-21 16:02:22 +0100

    Fee provider based on network (supports testnet fees)

--- src/store/factory/client.js
+++ src/store/factory/client.js
@@ -5,12 +5,14 @@ import BitcoinJsWalletProvider from '@liquality/bitcoin-js-wallet-provider'
 import BitcoinEsploraApiProvider from '@liquality/bitcoin-esplora-api-provider'
 import BitcoinEsploraSwapFindProvider from '@liquality/bitcoin-esplora-swap-find-provider'
 import BitcoinEarnFeeProvider from '@liquality/bitcoin-earn-fee-provider'
+import BitcoinRpcFeeProvider from '@liquality/bitcoin-rpc-fee-provider'
 
 import EthereumRpcProvider from '@liquality/ethereum-rpc-provider'
 import EthereumJsWalletProvider from '@liquality/ethereum-js-wallet-provider'
 import EthereumSwapProvider from '@liquality/ethereum-swap-provider'
 import EthereumScraperSwapFindProvider from '@liquality/ethereum-scraper-swap-find-provider'
 import EthereumGasStationFeeProvider from '@liquality/ethereum-gas-station-fee-provider'
+import EthereumRpcFeeProvider from '@liquality/ethereum-rpc-fee-provider'
 
 import EthereumErc20Provider from '@liquality/ethereum-erc20-provider'
 import EthereumErc20SwapProvider from '@liquality/ethereum-erc20-swap-provider'
@@ -89,12 +91,30 @@ const AdditionalSwapProviders = {
 }
 
 const FeeProviders = {
-  BTC: BitcoinEarnFeeProvider,
-  ETH: EthereumGasStationFeeProvider,
-  DAI: EthereumGasStationFeeProvider,
-  USDC: EthereumGasStationFeeProvider,
-  USDT: EthereumGasStationFeeProvider,
-  WBTC: EthereumGasStationFeeProvider
+  BTC: {
+    bitcoin: BitcoinEarnFeeProvider,
+    bitcoin_testnet: BitcoinRpcFeeProvider
+  },
+  ETH: {
+    mainnet: EthereumGasStationFeeProvider,
+    rinkeby: EthereumRpcFeeProvider
+  },
+  DAI: {
+    mainnet: EthereumGasStationFeeProvider,
+    rinkeby: EthereumRpcFeeProvider
+  },
+  USDC: {
+    mainnet: EthereumGasStationFeeProvider,
+    rinkeby: EthereumRpcFeeProvider
+  },
+  USDT: {
+    mainnet: EthereumGasStationFeeProvider,
+    rinkeby: EthereumRpcFeeProvider
+  },
+  WBTC: {
+    mainnet: EthereumGasStationFeeProvider,
+    rinkeby: EthereumRpcFeeProvider
+  },
 }
 
 const ERC20 = {
@@ -172,7 +192,7 @@ export const createClient = (network, mnemonic) => {
       ...AdditionalSwapArgs[asset]
     ))
 
-    client.addProvider(new FeeProviders[asset]())
+    client.addProvider(new FeeProviders[asset][NetworkArgs[asset]]())
 
     return {
       asset,
