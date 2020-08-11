320d194b761da50757991d48c05fb337fa3acec5
monokh <mnokhb@gmail.com>
2020-08-11 14:51:22 +0100

    Fix swap in quote step (before secret ready) not coninuing

--- src/store/actions/checkPendingSwaps.js
+++ src/store/actions/checkPendingSwaps.js
@@ -1,15 +1,8 @@
 import { NetworkAssets } from '../factory/client'
 
-const PENDING_STATES = [
-  'SECRET_READY',
-  'INITIATED',
-  'WAITING_FOR_CONFIRMATIONS',
-  'INITIATION_REPORTED',
-  'READY_TO_CLAIM',
-  'WAITING_FOR_CLAIM_CONFIRMATIONS',
-  'GET_REFUND',
-  'WAITING_FOR_REFUND_CONFIRMATIONS',
-  'READY_TO_SEND'
+const COMPLETED_STATES = [
+  'SUCCESS',
+  'REFUNDED'
 ]
 
 export const checkPendingSwaps = async ({ state, dispatch }) => {
@@ -20,7 +13,7 @@ export const checkPendingSwaps = async ({ state, dispatch }) => {
       state.history[network][walletId].forEach(order => {
         if (order.type !== 'SWAP') return
 
-        if (PENDING_STATES.includes(order.status)) {
+        if (!COMPLETED_STATES.includes(order.status)) {
           dispatch('performNextAction', { network, walletId, id: order.id })
         }
       })
