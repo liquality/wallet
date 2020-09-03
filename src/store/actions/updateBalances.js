fbb2d73d56855176154989fa84016670b0e4cc7b
Harsh Vakharia <harshjv@gmail.com>
2020-09-03 13:31:44 +0530

    set concurrency to 1 in .map network calls

--- src/store/actions/updateBalances.js
+++ src/store/actions/updateBalances.js
@@ -1,13 +1,13 @@
+import Bluebird from 'bluebird'
 import { NetworkAssets } from '../factory/client'
 
 export const updateBalances = async ({ commit, getters }, { network, walletId }) => {
   const assets = NetworkAssets[network]
 
-  return Promise.all(assets
-    .map(async asset => {
-      const addresses = await getters.client(network, walletId, asset).wallet.getUsedAddresses()
-      const balance = (await getters.client(network, walletId, asset).chain.getBalance(addresses)).toNumber()
+  return Bluebird.map(assets, async asset => {
+    const addresses = await getters.client(network, walletId, asset).wallet.getUsedAddresses()
+    const balance = (await getters.client(network, walletId, asset).chain.getBalance(addresses)).toNumber()
 
-      commit('UPDATE_BALANCE', { network, walletId, asset, balance })
-    }))
+    commit('UPDATE_BALANCE', { network, walletId, asset, balance })
+  }, { concurrency: 1 })
 }
