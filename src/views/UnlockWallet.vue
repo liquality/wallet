e10a2bb0a7995537386cb80df8138445e3731247
Bradley Suira <bradleysuira@gmail.com>
2020-12-07 13:03:08 -0500

    removed unused class style

--- src/views/UnlockWallet.vue
+++ src/views/UnlockWallet.vue
@@ -23,7 +23,7 @@
      <div class="login-footer">
         <p><router-link to="/onboarding/import">Forgot password? Import with seed phrase</router-link></p>
         <div class="footer-content">
-          <button class="btn btn-light btn-lg btn-block btn-icon" 
+          <button class="btn btn-light btn-lg btn-block btn-icon"
                   :disabled="loading"
                   @click="unlock">
             <SpinnerIcon v-if="loading" />
@@ -71,7 +71,5 @@ export default {
 </script>
 
 <style lang="scss">
-.unlock-wallet {
 
-}
 </style>
