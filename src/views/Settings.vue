9c9d8a46b90b77e6526db48f9c2f338806626c34
Bradley Suira <bradleysuira@gmail.com>
2020-12-22 19:16:04 -0500

    feat: set RSK as a label for RBTC asset network

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -18,7 +18,9 @@
         </div>
         <div class="setting-item_control">
           <select class="custom-select" @change="e => updateInjectEthereumAsset(e.target.value)">
-            <option v-for="asset in ethereumAssets" :key="asset" :selected="injectEthereumAsset === asset">{{ asset }}</option>
+            <option v-for="asset in ethereumAssets" :key="asset" :selected="injectEthereumAsset === asset">
+              {{ getEthereumAssetName(asset) }}
+            </option>
           </select>
         </div>
       </div>
@@ -57,6 +59,12 @@ export default {
     },
     updateInjectEthereumAsset (asset) {
       this.setEthereumInjectionAsset({ asset })
+    },
+    getEthereumAssetName (code) {
+      if (code === 'RBTC') {
+        return 'RSK'
+      }
+      return code
     }
   }
 }
