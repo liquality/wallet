ded789fc9d2e8cf2ca0e419edf717b602e2c958f
Bradley Suira <bradleysuira@gmail.com>
2021-01-23 12:57:19 -0500

    fix: added migration to update RSK to RBTC as injected ethereum asset

--- src/store/migrations.js
+++ src/store/migrations.js
@@ -35,6 +35,17 @@ const migrations = [
       }
       return { ...state, customTokens }
     }
+  },
+  { // Fix for RSK token injected asset
+    version: 4,
+    migrate: async (state) => {
+      if (state.injectEthereumAsset === 'RSK') {
+        const injectEthereumAsset = 'RBTC'
+        return { ...state, injectEthereumAsset }
+      }
+
+      return { ...state }
+    }
   }
 ]
 
