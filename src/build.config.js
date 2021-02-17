78539f6bd7b9ce36734c03b96080cda1b4fdc5cc
Bradley Suira <bradleysuira@gmail.com>
2021-02-17 12:28:08 -0500

    set the right testnet agent

--- src/build.config.js
+++ src/build.config.js
@@ -18,7 +18,7 @@ export default {
     ]
   },
   agentEndpoints: {
-    testnet: ['https://liquality.io/swap-testnet-dev/agent'],
+    testnet: ['https://liquality.io/swap-testnet/agent'],
     mainnet: ['https://liquality.io/swap/agent']
   },
   telegramUrl: 'https://t.me/liquality'
