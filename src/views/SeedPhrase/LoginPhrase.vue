40946b3fdfc395fd8069d318ba1614dd3a1d027a
unknown <sachscarl@gmail.com>
2021-03-17 14:13:39 -0400

    Repositioned checbox element to the left to center it more

--- src/views/SeedPhrase/LoginPhrase.vue
+++ src/views/SeedPhrase/LoginPhrase.vue
@@ -20,7 +20,7 @@
                     </ul>
                     </p>
                 </div>
-                <div class="form-group mt-5">
+                <div class="form-group mt-5 ml-1">
                   <div class="form-check phrase-check my-auto">
                     <input class="form-check-input" type="checkbox" value="" v-model="checkbox" id="checkbox">
                     <label class="form-check-label mt-1" for="checkbox">
