8f17ef25c4efd25ab5a5bafe95acec43b1969597
unknown <sachscarl@gmail.com>
2021-03-18 14:27:33 -0400

    More progress on the feature outlined below: 1) Added the initial buttons and set the values to each of the words in the seedlist 2) Created a new array, phraseSnip, that takes in elements clicked 3) Those elements are the word values, and those values are spread across the inputs up top 4) Took out old unneeded 'sign-in' class in past feature

--- src/views/SeedPhrase/ConfirmSeed.vue
+++ src/views/SeedPhrase/ConfirmSeed.vue
@@ -1,7 +1,7 @@
 <template>
 
-    <div class="login-wrapper confirm-seed h-100">
-        <div class="confirm-seed_logo-wrap mx-auto mt-1">
+    <div class="login-wrapper confirm-seed h-100 no-outer-pad">
+        <div class="confirm-seed_logo-wrap mt-5 mb-3 mx-auto">
             <img :src="logo"/>
         </div>
         <div class="confirm-seed_top">
@@ -10,36 +10,61 @@
         </div>
         <div class="confirm-seed_bottom">
             <div class="confirm-seed_bottom-top">
-                <form class="form import-wallet_seed">
-                    <div class="confirm-seed_inputs d-flex flex-row justify-content-evenly">
+                <div class="mx-auto mt-2">
+                <form class="form">
+                    <div class="confirm-seed_inputs d-flex flex-row justify-content-around">
                         <div>
                             <label>1<sub>st</sub> Word</label>
-                            <input type="text" class="form-control form-control-sm" autocomplete="off" required />
+                            <input type="text" class="form-control text-center w-75 mx-auto form-control-sm" v-model="phraseSnip[0]" autocomplete="off" required />
                         </div>
                         <div>
                             <label>5<sub>th</sub> Word</label>
-                            <input type="text" class="form-control form-control-sm" autocomplete="off" required />
+                            <input type="text" class="form-control text-center w-75 mx-auto form-control-sm" v-model="phraseSnip[1]" autocomplete="off" required />
                         </div>
                         <div>
                             <label>12<sub>th</sub> Word</label>
-                            <input type="text" class="form-control form-control-sm" autocomplete="off" required />
+                            <input type="text" class="form-control text-center w-75 mx-auto form-control-sm" v-model="phraseSnip[2]" autocomplete="off" required />
                         </div>
                     </div>
                 </form>
+                </div>
+                <div class="confirm-seed_selections mt-4 px-1 mx-auto">
+                    <div class="d-flex flex-row flex-wrap">
+                        <button class="btn btn-outline-primary w-25 btn-sm btn-block" v-for="word in seedList" :key="word" @click="() => onSelect(word)">{{ word }}</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-    
+
 </template>
 
 <script>
 
+import { mapState } from 'vuex'
 import LogoWallet from '@/assets/icons/logo_wallet.svg?inline'
 
 export default {
+    data () {
+        return {
+            phraseSnip: []
+        }
+    },
     computed: {
         logo () {
-            return LogoWallet
+        return LogoWallet
+        },
+        ...mapState(['wallets', 'activeWalletId']),
+        wallet: function () {
+        return this.wallets.find(wallet => wallet.id === this.activeWalletId)
+        },
+        seedList: function () {
+        return this.wallet.mnemonic.split(' ')
+        }
+    },
+        methods: {
+        onSelect(word) {
+        this.phraseSnip.push(word)
         }
     }
 }
@@ -48,17 +73,26 @@ export default {
 
 <style lang="scss">
 .confirm-seed {
+    padding: 0 !important;
+    overflow: hidden;
   &_logo-wrap {
     height: 100px;
     width: 100px;
   }
   &_bottom {
-      background: $color-text-secondary;
+    background: $color-text-secondary;
+    overflow-y: auto;
+  }
+  &_bottom-top {
+      overflow-x: hidden;
   }
   &_inputs {
       div {
           label {
               color: $color-text-primary;
+          }
+          input {
+              color: $color-text-primary !important;
           }
       }
   }
