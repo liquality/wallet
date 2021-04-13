712215961c5f53e9e8dbcbc6d7f06b0656c08d9d
monokh <mnokhb@gmail.com>
2021-04-13 16:09:19 +0100

    fix rsk

--- src/store/actions/sendTransaction.js
+++ src/store/actions/sendTransaction.js
@@ -3,13 +3,20 @@ import { createHistoryNotification } from '../../broker/notification'
 
 export const sendTransaction = async ({ dispatch, commit, getters }, { network, walletId, asset, to, amount, data, fee }) => {
   const client = getters.client(network, walletId, asset)
-  const tx = await client.chain.sendTransaction(to, amount, data, fee)
 
   // TODO: RSK GAS LIMIT PATCH - PROVIDER SHOULD BE ABLE TO SET CUSTOM LIMIT FROM DAPP - FIX IN NEXT RELEASE
-  if (asset === 'RBTC') {
+  const originalEstimateGas = client._providers[0].estimateGas
+  if (asset === 'RBTC' && data) {
     client._providers[0].estimateGas = async () => {
       return 2000000
     }
+  }
+
+  let tx
+  try {
+    tx = await client.chain.sendTransaction(to, amount, data, fee)
+  } finally {
+    client._providers[0].estimateGas = originalEstimateGas
   }
 
   const transaction = {
