ca9d8df25e75f208b23a728e4b08f55703df9c8b
Harsh Vakharia <harshjv@gmail.com>
2021-04-13 21:27:07 +0530

    chain background updates

--- src/background.js
+++ src/background.js
@@ -1,41 +1,41 @@
 import 'setimmediate'
 import { random } from 'lodash-es'
 import store from './store'
+import { wait } from './store/utils'
 
-let balanceInterval
-let fiatRatesInterval
-let marketDataInterval
+function asyncLoop (fn, delay) {
+  return wait(delay())
+    .then(() => fn())
+    .then(() => asyncLoop(fn, delay))
+}
 
-store.subscribe(({ type, payload }, state) => {
+store.subscribe(async ({ type, payload }, state) => {
   switch (type) {
     case 'CHANGE_ACTIVE_NETWORK':
-      store.dispatch('updateBalances', { network: state.activeNetwork, walletId: state.activeWalletId })
-      store.dispatch('updateMarketData', { network: state.activeNetwork })
+      await store.dispatch('updateBalances', { network: state.activeNetwork, walletId: state.activeWalletId })
+      await store.dispatch('updateMarketData', { network: state.activeNetwork })
       break
 
     case 'UNLOCK_WALLET':
-      store.dispatch('updateBalances', { network: state.activeNetwork, walletId: state.activeWalletId })
-      store.dispatch('updateFiatRates')
-      store.dispatch('updateMarketData', { network: state.activeNetwork })
-      store.dispatch('checkPendingActions', { walletId: state.activeWalletId })
+      await store.dispatch('updateBalances', { network: state.activeNetwork, walletId: state.activeWalletId })
+      await store.dispatch('updateFiatRates')
+      await store.dispatch('updateMarketData', { network: state.activeNetwork })
+      await store.dispatch('checkPendingActions', { walletId: state.activeWalletId })
 
-      if (!balanceInterval) {
-        balanceInterval = setInterval(() => {
-          store.dispatch('updateBalances', { network: state.activeNetwork, walletId: state.activeWalletId })
-        }, random(20000, 30000))
-      }
+      asyncLoop(
+        () => store.dispatch('updateBalances', { network: state.activeNetwork, walletId: state.activeWalletId }),
+        () => random(50000, 60000)
+      )
 
-      if (!fiatRatesInterval) {
-        fiatRatesInterval = setInterval(() => {
-          store.dispatch('updateFiatRates')
-        }, random(20000, 30000))
-      }
+      asyncLoop(
+        () => store.dispatch('updateFiatRates'),
+        () => random(50000, 60000)
+      )
 
-      if (!marketDataInterval) {
-        marketDataInterval = setInterval(() => {
-          store.dispatch('updateMarketData', { network: state.activeNetwork })
-        }, random(50000, 60000))
-      }
+      asyncLoop(
+        () => store.dispatch('updateMarketData', { network: state.activeNetwork }),
+        () => random(50000, 60000)
+      )
 
       break
   }
--- src/store/actions/enableAssets.js
+++ src/store/actions/enableAssets.js
@@ -1,6 +1,7 @@
 export const enableAssets = async ({ commit, dispatch }, { network, walletId, assets }) => {
   commit('ENABLE_ASSETS', { network, walletId, assets })
-  dispatch('updateBalances', { network, walletId })
-  dispatch('updateFiatRates')
-  dispatch('updateMarketData', { network })
+
+  await dispatch('updateBalances', { network, walletId })
+  await dispatch('updateFiatRates')
+  await dispatch('updateMarketData', { network })
 }
