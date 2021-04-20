6a1e4f325a0d2afc46d532055885a83158c22e7d
Bradley <bradleysuira@gmail.com>
2021-04-20 16:19:26 -0500

    change call to use update account balance for each account

--- src/store/actions/enableAssets.js
+++ src/store/actions/enableAssets.js
@@ -2,9 +2,10 @@ export const enableAssets = async ({ state, commit, dispatch }, { network, walle
   commit('ENABLE_ASSETS', { network, walletId, assets })
   const accounts = state.accounts[walletId]?.[network]
   accounts.forEach(account => {
-    commit('ENABLE_ACCOUNT_ASSETS', { network, walletId, assets, accountId: account.id })
+    const accountId = account.id
+    commit('ENABLE_ACCOUNT_ASSETS', { network, walletId, assets, accountId })
+    dispatch('updateAccountBalance', { network, walletId, accountId })
   })
-  dispatch('updateBalances', { network, walletId })
   dispatch('updateFiatRates')
   dispatch('updateMarketData', { network })
 }
