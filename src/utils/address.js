280b060cce0ca69881bd7cf0b5a26d1dbf486c87
unknown <sachscarl@gmail.com>
2021-05-11 18:01:29 -0400

    shortenaddress fixed

--- src/utils/address.js
+++ src/utils/address.js
@@ -1,5 +1,5 @@
 function shortenAddress (address) {
-  const prefix = address.includes('0x') ? '0x' : ''
+  const prefix = address.substring(0, 1).includes('0x') ? '0x' : ''
   return `${prefix}${address.replace('0x', '').substring(0, 4)}...${address.substring(address.length - 4)}`
 }
 
