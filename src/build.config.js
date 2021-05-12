5674aa5ae0980faeb148e00f38a66e1e1035f456
unknown <sachscarl@gmail.com>
2021-05-12 11:40:31 -0400

    shorten address fixed

--- src/build.config.js
+++ src/build.config.js
@@ -24,7 +24,7 @@ export default {
     ]
   },
   agentEndpoints: {
-    testnet: ['https://liquality.io/swap-testnet/agent'],
+    testnet: ['https://liquality.io/swap-testnet-dev/agent'],
     mainnet: ['https://liquality.io/swap/agent']
   },
   exploraApis: {
