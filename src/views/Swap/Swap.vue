b49e70e6955ff7387dd3f466357edacf2f1d6353
unknown <sachscarl@gmail.com>
2021-05-12 21:56:14 -0400

    No liquidity message displaying when no supposed to fix implemented

--- src/views/Swap/Swap.vue
+++ src/views/Swap/Swap.vue
@@ -429,7 +429,7 @@ export default {
       return this.$route.query.source || null
     },
     showNoLiquidityMessage () {
-      return !this.market || BN(this.min).gt(this.max)
+      return !this.market
     },
     sendAmount: {
       get () {
