d6bb63bef7d307b84917cbc8494333d16ea6185a
Bradley <bradleysuira@gmail.com>
2021-06-15 12:28:53 -0500

    feat: updated ledger integration to send the current network

--- src/store/actions/setUseLedgerLive.js
+++ src/store/actions/setUseLedgerLive.js
@@ -2,12 +2,14 @@ import {
   callToBridge
 } from '@/utils/ledger-bridge-provider'
 
-export const setUseLedgerLive = async ({ dispatch, commit }, { use }) => {
+export const setUseLedgerLive = async ({ state, commit }, { use }) => {
+  const { activeNetwork } = state
   await callToBridge({
+    network: activeNetwork,
+    app: 'SETTINGS',
     method: 'UPDATE-TRANSPORT-PREFERENCE',
     callType: 'ASYNC',
-    payload: { useLedgerLive: use },
-    app: 'SETTINGS'
+    payload: { useLedgerLive: use }
   })
   commit('SET_USE_LEDGER_LIVE', { use })
 }
