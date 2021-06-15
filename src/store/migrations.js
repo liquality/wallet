e501ff280a79f3efde0a493b815a55b02f569ace
monokh <mnokhb@gmail.com>
2021-06-15 16:17:46 +0100

    swaps: migration

--- src/store/migrations.js
+++ src/store/migrations.js
@@ -150,6 +150,27 @@ const migrations = [
 
       return { ...state, accounts, customTokens, history }
     }
+  },
+  { // Multi protocol swaps
+    version: 6,
+    migrate: async (state) => {
+      const walletId = state.activeWalletId
+
+      const migrateHistory = (state, network, walletId) => {
+        return state.history[network]?.[walletId].map(item => item.type === 'SWAP' ? { ...item, protocol: 'liquality' } : item)
+      }
+
+      const history = {
+        mainnet: {
+          [walletId]: migrateHistory(state, 'mainnet', walletId)
+        },
+        testnet: {
+          [walletId]: migrateHistory(state, 'testnet', walletId)
+        }
+      }
+
+      return { ...state, history }
+    }
   }
 ]
 
