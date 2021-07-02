724175920993f5d43b4763579dabb0e3bd662458
Raja Beemi <beemi.raja@gmail.com>
2021-07-02 10:56:35 +0100

    Added 24 seed words import wallet

--- tests/integration/testnet/import_wallet_testnet.spec.js
+++ tests/integration/testnet/import_wallet_testnet.spec.js
@@ -52,7 +52,7 @@ describe('Liquality wallet- Import wallet', async () => {
     const assetsCount = await overviewPage.GetTotalAssets(page)
     expect(assetsCount, 'Total assets in TESTNET should be 6').contain('6 Assets')
   })
-  it('Import wallet and see balance', async () => {
+  it('Import wallet with (12 seed words) and see balance', async () => {
     // Import wallet option
     await homePage.ClickOnImportWallet(page)
     // Enter seed words and submit
@@ -76,6 +76,31 @@ describe('Liquality wallet- Import wallet', async () => {
     const totalAmount = await overviewPage.GetTotalLiquidity(page)
     expect(parseInt(totalAmount), 'Funds in my wallet should be greater than 2000 USD').greaterThanOrEqual(2000)
     console.log('After Import wallet, the funds in the wallet:', totalAmount)
+  })
+  it('Import wallet with (24 seed words) and see balance', async () => {
+    // Import wallet option
+    await homePage.ClickOnImportWallet(page)
+    // Enter seed words and submit, select 24 seed option
+    await page.waitForSelector('#word_button_group', { visible: true })
+    await page.click('#twenty_four_words_option')
+    const seedWords = await page.$$eval('#import_wallet_word', (el) => el.length)
+    expect(seedWords).equals(24)
+    // Enter 24 seed words
+    await homePage.EnterSeedWords(page, 24)
+    // Create a password & submit
+    await passwordPage.SubmitPasswordDetails(page, password)
+    // overview page
+    await overviewPage.HasOverviewPageLoaded(page)
+    // Select testnet
+    await overviewPage.SelectNetwork(page, 'testnet')
+    // check Send & Swap & Receive options have been displayed
+    await overviewPage.ValidateSendSwipeReceiveOptions(page)
+    // validate the testnet asserts count
+    const assetsCount = await overviewPage.GetTotalAssets(page)
+    expect(assetsCount, 'Total assets in TESTNET should be 6').contain('6 Assets')
+    // Check the currency
+    expect(await overviewPage.GetCurrency(page),
+      'Wallet stats has currency should be USD').contain('USD')
   })
   it('Import wallet,lock wallet and unlock wallet-["mainnet"]', async () => {
     // Import wallet option
