21a1ff9b07a06484bda43a884e9685568292b5e0
Raja Beemi <beemi.raja@gmail.com>
2021-07-05 20:43:10 +0100

    Added Settings tests

--- tests/integration/Pages/SearchAssetPage.js
+++ tests/integration/Pages/SearchAssetPage.js
@@ -12,10 +12,11 @@ class SearchAssetPage {
   async SearchForAnAsset (page, asset) {
     await page.waitForSelector('#search_for_a_currency_search', { visible: true })
     await page.type('#search_for_a_currency_search', asset)
-    await page.waitForTimeout(2000)
     const assertListItems = await page.$$('#assert_list_item')
+    await page.waitForTimeout(10000)
     switch (asset) {
       case 'BTC': {
+        await page.waitForSelector('#BITCOIN',{visible: true})
         await page.click('#BITCOIN')
         break
       }
--- tests/integration/testnet/swipe_testnet.spec.js
+++ tests/integration/testnet/swipe_testnet.spec.js
@@ -32,7 +32,7 @@ describe('Liquality wallet SWIPE feature', async () => {
     }
   })
 
-  it('SWAP BTC to ETH', async () => {
+  it.only('SWAP BTC to ETH', async () => {
     const asset1 = 'BTC'
     const asset2 = 'ETH'
 
