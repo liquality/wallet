b86b3660f0a188535c7ea61bc6d3d2ac576c312a
monokh <mnokhb@gmail.com>
2021-07-08 16:24:27 +0100

    New swap provider

--- src/utils/swaps.js
+++ src/utils/swaps.js
@@ -2,7 +2,8 @@ import buildConfig from '../build.config'
 
 export const SwapProviderType = {
   LIQUALITY: 'LIQUALITY',
-  UNISWAPV2: 'UNISWAPV2'
+  UNISWAPV2: 'UNISWAPV2',
+  THORCHAIN: 'THORCHAIN'
 }
 
 export function getSwapProviderConfig (network, providerId) {
