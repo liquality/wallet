aca87c77b910707c9da25c8301106fca266dfbbf
Bradley <bradleysuira@gmail.com>
2021-08-03 21:47:57 -0500

    fix: added a check to know when we have a current external request from dapps to not duplication windows/popups

--- src/store/migrations.js
+++ src/store/migrations.js
@@ -189,7 +189,7 @@ const migrations = [
   { // Inject ethereum asset -> chain
     version: 9,
     migrate: async (state) => {
-      const injectEthereumChain = cryptoassets[state.injectEthereumAsset].chain
+      const injectEthereumChain = cryptoassets[state.injectEthereumAsset]?.chain || 'ethereum'
       delete state.injectEthereumAsset
 
       return { ...state, injectEthereumChain }
