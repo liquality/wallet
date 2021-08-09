f8b0a2fac4b93a30980e919edcbb81a4557b6e33
unknown <sachscarl@gmail.com>
2021-08-09 10:51:14 -0400

    swapped out dev

--- src/build.config.js
+++ src/build.config.js
@@ -46,7 +46,7 @@ export default {
         name: 'Liquality',
         icon: 'liquality.svg',
         type: SwapProviderType.LIQUALITY,
-        agent: process.env.VUE_APP_AGENT_TESTNET_URL || 'https://liquality.io/swap-testnet-dev/agent'
+        agent: process.env.VUE_APP_AGENT_TESTNET_URL || 'https://liquality.io/swap-testnet/agent'
       },
       uniswapV2: {
         name: 'Uniswap V2',
