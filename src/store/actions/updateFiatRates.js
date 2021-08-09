8095754f09013b9990bf0d1b40dc8b5c93aea1e8
monokh <mnokhb@gmail.com>
2021-08-09 18:57:11 +0100

    Update fiat rates: remove console log

--- src/store/actions/updateFiatRates.js
+++ src/store/actions/updateFiatRates.js
@@ -1,7 +1,6 @@
 import { getPrices } from '../utils'
 
 export const updateFiatRates = async ({ commit }, { assets }) => {
-  console.log('updating fiat rates', assets)
   const fiatRates = await getPrices(assets, 'usd')
 
   commit('UPDATE_FIAT_RATES', { fiatRates })
