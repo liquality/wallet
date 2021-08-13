c43bfe58d7f243d2f976424347b7dabbec4fc137
AndonMitev <mitevandon94@gmail.com>
2021-08-13 11:34:32 +0300

    Feat enable custom fee in Permission Send

--- src/views/PermissionSend.vue
+++ src/views/PermissionSend.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="permission-send wrapper form text-center">
-    <div class="wrapper_top form">
+    <div v-if="currentStep === 'inputs'" class="wrapper_top form">
       <div v-if="error" class="mt-4 text-danger"><strong>Error:</strong> {{ error }}</div>
       <div v-if="isApprove">
         <div class="form-group">
@@ -22,6 +22,9 @@
         <label>To</label>
         <p class="confirm-value">{{shortAddress}}</p>
       </div>
+      <div class="form-group">
+        <label>Transaction fee {{feeInUsdValue}} USD</label>
+      </div>
       <div v-if="data" class="permission-send_data">
         <label @click="toggleshowData"><ChevronDown v-if="showData" class="permission-send_data_icon-down" /><ChevronRight class="permission-send_data_icon-right" v-else />Data</label>
         <div class="permission-send_data_code" v-if="showData">{{data}}</div>
@@ -36,13 +39,28 @@
             :asset="asset"
             v-model="selectedFee"
             v-bind:fees="assetFees"
-            v-bind:fiatRates="fiatRates"/>
+            v-bind:fiatRates="fiatRates"
+            @custom-selected="onCustomFeeSelected(asset)"
+            />
         </div>
       </div>
     </div>
     
     
-    <div class="wrapper_bottom">
+  <div class="send" v-else-if="currentStep === 'custom-fees'">
+      <CustomFees
+        @apply="applyCustomFee"
+        @update="setCustomFee"
+        @cancel="cancelCustomFee"
+        :asset="assetChain"
+        :selected-fee="selectedFee"
+        :fees="assetFees"
+        :totalFees="maxOptionActive ? maxSendFees : sendFees"
+        :fiatRates="fiatRates"
+      />
+    </div>
+
+    <div v-if="currentStep === 'inputs'" class="wrapper_bottom">
       <div class="button-group">
         <button class="btn btn-light btn-outline-primary btn-lg" @click="reply(false)">Cancel</button>
         <button class="btn btn-primary btn-lg btn-icon" @click.stop="reply(true)" :disabled="loading">
@@ -55,19 +73,24 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+import { mapState, mapGetters, mapActions } from 'vuex'
 import cryptoassets from '@/utils/cryptoassets'
-import { unitToCurrency } from '@liquality/cryptoassets'
+import { currencyToUnit, unitToCurrency } from '@liquality/cryptoassets'
 import FeeSelector from '@/components/FeeSelector'
+import CustomFees from '@/components/CustomFees'
 import { prettyBalance, prettyFiatBalance } from '@/utils/coinFormatter'
 import { getNativeAsset, getAssetColorStyle } from '@/utils/asset'
 import { parseTransactionData } from '@/utils/txData'
 import { tokenDetailProviders } from '@/utils/asset'
 import { shortenAddress } from '@/utils/address'
+import {
+  getSendFee
+} from '@/utils/fees'
 import SpinnerIcon from '@/assets/icons/spinner.svg'
 import ChevronDown from '@/assets/icons/chevron_down.svg'
 import ChevronRight from '@/assets/icons/chevron_right.svg'
 import BN from 'bignumber.js'
+import _ from 'lodash'
 
 const TRANSACTION_TYPES = {
   approve: 'Allow',
@@ -79,19 +102,24 @@ export default {
     SpinnerIcon,
     ChevronDown,
     ChevronRight,
-    FeeSelector
+    FeeSelector,
+    CustomFees
   },
   data () {
     return {
+      sendFees: {},
+      maxSendFees: {},
       showData: false,
       selectedFee: 'average',
       error: null,
       loading: false,
       replied: false,
+      maxOptionActive: false,
       symbol: '',
       label: '',
       subLabel: '',
-      isApprove: false
+      isApprove: false,
+      currentStep: 'inputs'
     }
   },
   methods: {
@@ -102,16 +130,27 @@ export default {
     toggleshowData () {
       this.showData = !this.showData
     },
+    onCustomFeeSelected () {
+      this.currentStep = 'custom-fees'
+    },
+    cancelCustomFee () {
+      this.currentStep = 'inputs'
+      this.selectedFee = 'average'
+    },
     async getSymbol() {
       if(this.assetChain === 'ETH') {
-          const data = await tokenDetailProviders.ethereum.getDetails(this.request.args[0].to)
+        try {
+        const data = await tokenDetailProviders.ethereum.getDetails(this.request.args[0].to)
           this.symbol = data.symbol
+        } catch (err) {
+          this.symbol = 'ETH'
+        }
+      } else {
+        this.symbol = this.asset
       }
-      return '';
     },
     async getLabel() {
       const txType = parseTransactionData(this.request.args[0]?.data)?.name || 'send'
-      
       if (txType === 'approve' && chrome.tabs?.query) {
         this.isApprove = true;
         
@@ -153,10 +192,62 @@ export default {
       } finally {
         this.loading = false
       }
-    }
+    },
+
+    async _updateSendFees (amount) {
+      const getMax = amount === undefined
+      if (this.feesAvailable) {
+        const sendFees = {}
+        for (const [speed, fee] of Object.entries(this.assetFees)) {
+          const gas = BN(this.request.args[0].gas, 16)
+          const feePerGas = BN(fee.fee).div(1e9)
+          const txCost = gas.times(feePerGas)
+          sendFees[speed] = txCost
+        }
+        
+        if (getMax) {
+          this.maxSendFees = sendFees
+        } else {
+          this.sendFees = sendFees
+        }
+      }
+      
+    },
+    applyCustomFee ({ fee }) {
+      const presetFee = Object.entries(this.assetFees).find(([speed, speedFee]) => speed !== 'custom' && speedFee.fee === fee)
+      if (presetFee) {
+        const [speed] = presetFee
+        this.selectedFee = speed
+        this.customFee = null
+      } else {
+        this.updateMaxSendFees()
+        this.updateSendFees(this.amount)
+        this.customFee = fee
+        this.selectedFee = 'custom'
+      }
+      this.currentStep = 'inputs'
+    },
+    updateSendFees: _.debounce(async function (amount) {
+      await this._updateSendFees(amount)
+    }, 800),
+    async updateMaxSendFees () {
+      await this._updateSendFees()
+    },
+    setCustomFee: _.debounce(async function ({ fee }) {
+      this.customFee = fee
+      if (this.maxOptionActive) {
+        this.updateMaxSendFees()
+      } else {
+        this.updateSendFees(this.amount)
+      }
+    }, 800),
+    
   },
   computed: {
     ...mapState(['activeNetwork', 'activeWalletId', 'fees', 'fiatRates']),
+    ...mapGetters([
+      'client'
+    ]),
     asset () {
       return this.request.asset
     },
@@ -182,7 +273,19 @@ export default {
       return this.request.args[0].data
     },
     assetFees () {
-      return this.fees[this.activeNetwork]?.[this.activeWalletId]?.[this.assetChain]
+      const assetFees = {}
+      if (this.customFee) {
+        assetFees.custom = { fee: this.customFee }
+      }
+
+      const fees = this.fees[this.activeNetwork]?.[this.activeWalletId]?.[
+        this.assetChain
+      ]
+      if (fees) {
+        Object.assign(assetFees, fees)
+      }
+
+      return assetFees
     },
     feeInUsdValue() {
       const gas = BN(this.request.args[0].gas, 16)
@@ -199,12 +302,14 @@ export default {
         ...this.$route.query,
         args: JSON.parse(this.$route.query.args)
       }
-    }
+    },
   },
-  created () {
-    this.getSymbol()
-    this.getLabel()
-    this.updateFees({ asset: this.asset })
+  async created () {
+    await this.getSymbol()
+    // this.getLabel()
+    await this.updateFees({ asset: this.asset })
+    await this.updateSendFees(0)
+    await this.updateMaxSendFees()
   },
   beforeDestroy () {
     // TODO: need to reply correctly when window is closed
@@ -216,6 +321,35 @@ export default {
 </script>
 
 <style lang="scss">
+.send {
+  &_asset {
+    &.input-group {
+      align-items: center;
+    }
+
+    &_icon {
+      margin-right: 4px;
+    }
+
+    input {
+      text-align: right;
+      margin-left: 12px;
+    }
+  }
+  &_fees {
+    display: flex;
+    align-items: center;
+    font-weight: bold;
+    margin: 6px 0;
+    .fee-selector {
+      margin-left: 6px;
+    }
+    .custom-fees {
+      font-weight: normal;
+    }
+
+  }
+}
 .permission-send {
   &_fees {
     text-align: center;
