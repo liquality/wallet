ba3156d9b553f8b3d83459e08f8e3842ad70a0f2
tima-t <tito.marchev@gmail.com>
2021-08-25 00:54:16 +0300

    refactor: optimize code and remove repeating code

--- src/swaps/liqualityboost/LiqualityBoostSwapProvider.js
+++ src/swaps/liqualityboost/LiqualityBoostSwapProvider.js
@@ -1,11 +1,12 @@
 import BN from 'bignumber.js'
 import { SwapProvider } from '../SwapProvider'
 import { unitToCurrency, assets } from '@liquality/cryptoassets'
-import { sha256 } from '@liquality/crypto'
-import { withLock, withInterval } from '../../store/actions/performNextAction/utils'
+import { withInterval } from '../../store/actions/performNextAction/utils'
 import { prettyBalance } from '../../utils/coinFormatter'
 import { isERC20, getNativeAsset } from '@/utils/asset'
 import { createSwapProvider } from '../../store/factory/swapProvider'
+import { LiqualitySwapProvider } from '../liquality/LiqualitySwapProvider'
+import { OneinchSwapProvider } from '../oneinch/OneinchSwapProvider'
 
 const slippagePercentage = 3
 
@@ -22,78 +23,36 @@ class LiqualityBoostSwapProvider extends SwapProvider {
 
   async getQuote ({ network, from, to, amount }) {
     if (isERC20(from) || !isERC20(to) || amount <= 0) return null
-    const toNativeAsset = getNativeAsset(to)
-    const quote = await this.liqualitySwapProvider.getQuote({ network, from, to: toNativeAsset, amount })
+    const bridgeAsset = getNativeAsset(to)
+    const quote = await this.liqualitySwapProvider.getQuote({ network, from, to: bridgeAsset, amount })
     if (!quote) return null
-    const toNativeAssetQuantity = unitToCurrency(assets[toNativeAsset], quote.toAmount)
-    const finalQuote = await this.oneinchSwapProvider.getQuote({ network, from: toNativeAsset, to, amount: toNativeAssetQuantity.toNumber() })
+    const bridgeAssetQuantity = unitToCurrency(assets[bridgeAsset], quote.toAmount)
+    const finalQuote = await this.oneinchSwapProvider.getQuote({ network, from: bridgeAsset, to, amount: bridgeAssetQuantity.toNumber() })
     if (!finalQuote) return null
     return {
       from,
       to,
       fromAmount: quote.fromAmount,
       toAmount: finalQuote.toAmount,
-      toNativeAsset,
-      toNativeAssetAmount: quote.toAmount
+      bridgeAsset,
+      bridgeAssetAmount: quote.toAmount
     }
   }
 
   async newSwap ({ network, walletId, quote: _quote }) {
-    const lockedQuote = await this.liqualitySwapProvider._getQuote({ from: _quote.from, to: _quote.toNativeAsset, amount: _quote.fromAmount })
-    const quote = {
-      ...lockedQuote,
-      ..._quote,
-      toNativeAssetAmount: lockedQuote.toAmount
-    }
-    if (await this.liqualitySwapProvider.hasQuoteExpired({ network, walletId, swap: quote })) {
-      throw new Error('The quote is expired.')
-    }
-
-    quote.fromAddress = await this.getSwapAddress(network, walletId, quote.from, quote.fromAccountId)
-    quote.toAddress = await this.getSwapAddress(network, walletId, quote.toNativeAsset, quote.toAccountId)
-    const fromClient = this.getClient(network, walletId, quote.from, quote.fromAccountId)
-
-    const message = [
-      'Creating a swap with following terms:',
-      `Send: ${quote.fromAmount} (lowest denomination) ${quote.from}`,
-      `Receive: ${quote.toAmount} (lowest denomination) ${quote.to}`,
-      `My ${quote.from} Address: ${quote.fromAddress}`,
-      `My ${quote.to} Address: ${quote.toAddress}`,
-      `Counterparty ${quote.from} Address: ${quote.fromCounterPartyAddress}`,
-      `Counterparty ${quote.to} Address: ${quote.toCounterPartyAddress}`,
-      `Timestamp: ${quote.swapExpiration}`
-    ].join('\n')
-
-    const messageHex = Buffer.from(message, 'utf8').toString('hex')
-    const secret = await fromClient.swap.generateSecret(messageHex)
-    const secretHash = sha256(secret)
-
-    const fromFundTx = await fromClient.swap.initiateSwap(
-      {
-        value: BN(quote.fromAmount),
-        recipientAddress: quote.fromCounterPartyAddress,
-        refundAddress: quote.fromAddress,
-        secretHash: secretHash,
-        expiration: quote.swapExpiration
-      },
-      quote.fee
-    )
-
+    const result = await this.liqualitySwapProvider.newSwap({ network, walletId, quote: { ..._quote, to: _quote.bridgeAsset } })
     return {
-      ...quote,
-      status: 'INITIATED',
-      secret,
-      secretHash,
-      fromFundHash: fromFundTx.hash,
-      fromFundTx,
-      slippage: slippagePercentage * 100
+      ...result,
+      ..._quote,
+      slippage: slippagePercentage * 100,
+      bridgeAssetAmount: result.toAmount
     }
   }
 
   async estimateFees ({ network, walletId, asset, txType, quote, feePrices, max }) {
-    const liqualityFees = await this.liqualitySwapProvider.estimateFees({ network, walletId, asset, txType, quote: { ...quote, to: quote.toNativeAsset, toAmount: quote.toNativeAssetAmount }, feePrices, max })
+    const liqualityFees = await this.liqualitySwapProvider.estimateFees({ network, walletId, asset, txType, quote: { ...quote, to: quote.bridgeAsset, toAmount: quote.bridgeAssetAmount }, feePrices, max })
     if (isERC20(asset) && txType === LiqualityBoostSwapProvider.txTypes.SWAP_CLAIM) {
-      const oneinchFees = await this.oneinchSwapProvider.estimateFees({ network, walletId, asset, txType: LiqualityBoostSwapProvider.txTypes.SWAP, quote: { ...quote, from: quote.toNativeAsset, fromAmount: quote.toNativeAssetAmount, fromAccountId: quote.toAccountId, slippagePercentage }, feePrices, max })
+      const oneinchFees = await this.oneinchSwapProvider.estimateFees({ network, walletId, asset, txType: LiqualityBoostSwapProvider.txTypes.SWAP, quote: { ...quote, from: quote.bridgeAsset, fromAmount: quote.bridgeAssetAmount, fromAccountId: quote.toAccountId, slippagePercentage }, feePrices, max })
       const totalFees = {}
       for (const key in oneinchFees) {
         totalFees[key] = BN(oneinchFees[key]).plus(liqualityFees[key])
@@ -105,184 +64,72 @@ class LiqualityBoostSwapProvider extends SwapProvider {
 
   async finalizeLiqualitySwapAndStartOneinch ({ swap, network, walletId }) {
     const result = await this.liqualitySwapProvider.waitForClaimConfirmations({ swap, network, walletId })
-    if (result?.status === 'SUCCESS') return { endTime: Date.now(), status: 'SEND_ONEINCH_SWAP' }
+    if (result?.status === 'SUCCESS') return { endTime: Date.now(), status: 'APPROVE_CONFIRMED' }
   }
 
   async performNextSwapAction (store, { network, walletId, swap }) {
     let updates
-    const swapLiqualityFormat = { ...swap, to: swap.toNativeAsset, toAmount: swap.toNativeAssetAmount, slippagePercentage }
-    const swapOneInchFormat = { ...swap, from: swap.toNativeAsset, fromAmount: swap.toNativeAssetAmount, fromAccountId: swap.toAccountId, slippagePercentage }
-    switch (swap.status) {
-      case 'INITIATED':
-        updates = await this.liqualitySwapProvider.reportInitiation({ swap: swapLiqualityFormat, network, walletId })
-        break
-
-      case 'INITIATION_REPORTED':
-        updates = await withInterval(async () => this.liqualitySwapProvider.confirmInitiation({ swap: swapLiqualityFormat, network, walletId }))
-        break
-
-      case 'INITIATION_CONFIRMED':
-        updates = await withLock(store, { item: swap, network, walletId, asset: swap.from },
-          async () => this.liqualitySwapProvider.fundSwap({ swap: swapLiqualityFormat, network, walletId }))
-        break
-
-      case 'FUNDED':
-        updates = await withInterval(async () => this.liqualitySwapProvider.findCounterPartyInitiation({ swap: swapLiqualityFormat, network, walletId }))
-        break
-
-      case 'CONFIRM_COUNTER_PARTY_INITIATION':
-        updates = await withInterval(async () => this.liqualitySwapProvider.confirmCounterPartyInitiation({ swap: swapLiqualityFormat, network, walletId }))
-        break
-
-      case 'READY_TO_CLAIM':
-        updates = await withLock(store, { item: swap, network, walletId, asset: swap.to },
-          async () => this.liqualitySwapProvider.claimSwap({ swap: swapLiqualityFormat, network, walletId }))
-        break
-
-      case 'WAITING_FOR_CLAIM_CONFIRMATIONS':
-        updates = await withInterval(async () => this.finalizeLiqualitySwapAndStartOneinch({ swap: swapLiqualityFormat, network, walletId }))
-        break
-
-      case 'WAITING_FOR_REFUND':
-        updates = await withInterval(async () => this.liqualitySwapProvider.waitForRefund({ swap: swapLiqualityFormat, network, walletId }))
-        break
-
-      case 'GET_REFUND':
-        updates = await withLock(store, { item: swap, network, walletId, asset: swap.from },
-          async () => this.liqualitySwapProvider.refundSwap({ swap: swapLiqualityFormat, network, walletId }))
-        break
-
-      case 'WAITING_FOR_REFUND_CONFIRMATIONS':
-        updates = await withInterval(async () => this.liqualitySwapProvider.waitForRefundConfirmations({ swap: swapLiqualityFormat, network, walletId }))
-        break
-      case 'SEND_ONEINCH_SWAP':
-        updates = await withLock(store, { item: swap, network, walletId, asset: swap.from },
-          async () => this.oneinchSwapProvider.sendSwap({ quote: swapOneInchFormat, network, walletId }))
-        break
-      case 'WAITING_FOR_SWAP_CONFIRMATIONS':
-        updates = await withInterval(async () => this.oneinchSwapProvider.waitForSwapConfirmations({ swap: swapOneInchFormat, network, walletId }))
-        break
+    const swapLiqualityFormat = { ...swap, to: swap.bridgeAsset, toAmount: swap.bridgeAssetAmount, slippagePercentage }
+    const swapOneInchFormat = { ...swap, from: swap.bridgeAsset, fromAmount: swap.bridgeAssetAmount, fromAccountId: swap.toAccountId, slippagePercentage }
+    if (swap.status === 'WAITING_FOR_CLAIM_CONFIRMATIONS') {
+      updates = await withInterval(async () => this.finalizeLiqualitySwapAndStartOneinch({ swap: swapLiqualityFormat, network, walletId }))
+    } else {
+      updates = await this.liqualitySwapProvider.performNextSwapAction(store, { network, walletId, swap: swapLiqualityFormat })
     }
 
+    if (!updates) {
+      updates = await this.oneinchSwapProvider.performNextSwapAction(store, { network, walletId, swap: swapOneInchFormat })
+    }
     return updates
   }
 
   static txTypes = {
-    SWAP_INITIATION: 'SWAP_INITIATION',
-    SWAP_CLAIM: 'SWAP_CLAIM',
-    SWAP: 'SWAP'
+    ...LiqualitySwapProvider.txTypes,
+    ...OneinchSwapProvider.txTypes
   }
 
   static statuses = {
-    INITIATED: {
-      step: 0,
-      label: 'Locking {from}',
-      filterStatus: 'PENDING'
-    },
-    INITIATION_REPORTED: {
-      step: 0,
-      label: 'Locking {from}',
-      filterStatus: 'PENDING',
-      notification () {
-        return {
-          message: 'Swap initiated'
-        }
-      }
-    },
-    INITIATION_CONFIRMED: {
-      step: 0,
-      label: 'Locking {from}',
-      filterStatus: 'PENDING'
-    },
+    ...LiqualitySwapProvider.statuses,
+    ...OneinchSwapProvider.statuses,
     FUNDED: {
-      step: 1,
-      label: 'Locking {toNativeAsset}',
-      filterStatus: 'PENDING'
+      ...LiqualitySwapProvider.statuses.FUNDED,
+      label: 'Locking {bridgeAsset}'
     },
     CONFIRM_COUNTER_PARTY_INITIATION: {
-      step: 1,
-      label: 'Locking {toNativeAsset}',
-      filterStatus: 'PENDING',
+      ...LiqualitySwapProvider.statuses.CONFIRM_COUNTER_PARTY_INITIATION,
+      label: 'Locking {bridgeAsset}',
       notification (swap) {
         return {
-          message: `Counterparty sent ${prettyBalance(swap.toAmount, swap.to)} ${swap.to} to escrow`
+          message: `Counterparty sent ${prettyBalance(swap.bridgeAssetAmount, swap.bridgeAsset)} ${swap.bridgeAsset} to escrow`
         }
       }
     },
     READY_TO_CLAIM: {
-      step: 2,
-      label: 'Claiming {toNativeAsset}',
-      filterStatus: 'PENDING',
-      notification () {
-        return {
-          message: 'Claiming funds'
-        }
-      }
+      ...LiqualitySwapProvider.statuses.READY_TO_CLAIM,
+      label: 'Claiming {bridgeAsset}'
     },
     WAITING_FOR_CLAIM_CONFIRMATIONS: {
-      step: 2,
-      label: 'Claiming {toNativeAsset}',
-      filterStatus: 'PENDING'
+      ...LiqualitySwapProvider.statuses.WAITING_FOR_CLAIM_CONFIRMATIONS,
+      label: 'Claiming {bridgeAsset}'
     },
-    WAITING_FOR_REFUND: {
-      step: 2,
-      label: 'Pending Refund',
-      filterStatus: 'PENDING'
-    },
-    GET_REFUND: {
-      step: 2,
-      label: 'Refunding {from}',
-      filterStatus: 'PENDING'
-    },
-    WAITING_FOR_REFUND_CONFIRMATIONS: {
-      step: 2,
-      label: 'Refunding {from}',
-      filterStatus: 'PENDING'
-    },
-    REFUNDED: {
+    APPROVE_CONFIRMED: {
+      ...OneinchSwapProvider.statuses.APPROVE_CONFIRMED,
       step: 3,
-      label: 'Refunded',
-      filterStatus: 'REFUNDED',
-      notification (swap) {
-        return {
-          message: `Swap refunded, ${prettyBalance(swap.fromAmount, swap.from)} ${swap.from} returned`
-        }
-      }
-    },
-    SEND_ONEINCH_SWAP: {
-      step: 4,
-      label: 'Swapping {toNativeAsset} for {to}',
-      filterStatus: 'PENDING'
+      label: 'Swapping {bridgeAsset} for {to}'
     },
     WAITING_FOR_SWAP_CONFIRMATIONS: {
-      step: 4,
-      label: 'Waiting Swap Confirmation {to}',
-      filterStatus: 'PENDING',
-      notification () {
-        return {
-          message: 'Engaging oneinch'
-        }
-      }
+      ...OneinchSwapProvider.statuses.WAITING_FOR_SWAP_CONFIRMATIONS,
+      step: 3
     },
     SUCCESS: {
-      step: 5,
-      label: 'Completed',
-      filterStatus: 'COMPLETED',
-      notification (swap) {
-        return {
-          message: `Swap completed, ${prettyBalance(swap.toAmount, swap.to)} ${swap.to} ready to use`
-        }
-      }
-    },
-    QUOTE_EXPIRED: {
-      step: 5,
-      label: 'Quote Expired',
-      filterStatus: 'REFUNDED'
+      ...LiqualitySwapProvider.statuses.SUCCESS,
+      step: 4,
+      label: 'Completed'
     }
   }
 
   static fromTxType = LiqualityBoostSwapProvider.txTypes.SWAP_INITIATION
-  static toTxType = LiqualityBoostSwapProvider.txTypes.SWAP_CLAIM
+  static toTxType = LiqualityBoostSwapProvider.txTypes.SWAP
 
   static totalSteps = 5
 }
--- src/swaps/liqualityboost/SwapDetails.vue
+++ src/swaps/liqualityboost/SwapDetails.vue
@@ -2,9 +2,10 @@
   <div class="liqualitybost-swap-details uniswap-swap-details">
     <div class="row"><div class="col">Slippage: {{ slippagePercent }}% </div></div>
     <div class="row" v-if="item.fromFundHash"><div class="col">Wallet Inititation Transaction: <a :href="getExplorerLink(item.fromFundHash, item.from)" target="_blank">{{ item.fromFundHash }}</a></div></div>
-    <div class="row" v-if="item.toFundHash"><div class="col">Liquality Inititation Transaction: <a :href="getExplorerLink(item.toFundHash, item.toNativeAsset)" target="_blank">{{ item.toFundHash }}</a></div></div>
-    <div class="row" v-if="item.toClaimHash"><div class="col">Walelt Claim Transaction: <a :href="getExplorerLink(item.toClaimHash, item.toNativeAsset)" target="_blank">{{ item.toClaimHash }}</a></div></div>
-    <div class="row" v-if="item.swapTxHash"><div class="col">1inch Swap Transaction: <a :href="getExplorerLink(item.swapTxHash, item.toNativeAsset)" target="_blank">{{ item.swapTxHash }}</a></div></div>
+    <div class="row" v-if="item.toFundHash"><div class="col">Liquality Inititation Transaction: <a :href="getExplorerLink(item.toFundHash, item.bridgeAsset)" target="_blank">{{ item.toFundHash }}</a></div></div>
+    <div class="row" v-if="item.toClaimHash"><div class="col">Wallet Claim Transaction: <a :href="getExplorerLink(item.toClaimHash, item.bridgeAsset)" target="_blank">{{ item.toClaimHash }}</a></div></div>
+    <div class="row" v-if="item.swapTxHash"><div class="col">1inch Swap Transaction: <a :href="getExplorerLink(item.swapTxHash, item.bridgeAsset)" target="_blank">{{ item.swapTxHash }}</a></div></div>
+    <div class="row" v-if="item.refundHash"><div class="col">Refund Transaction: <a :href="getExplorerLink(item.refundHash, item.from)" target="_blank">{{ item.refundHash }}</a></div></div>
     <div class="text-muted text-center small-12">Actions</div>
     <div class="text-danger text-center">
       <span class="cursor-pointer mr-3" v-if="item.error" @click="$emit('retrySwap')">Retry</span>
