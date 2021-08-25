ddb64c9d14c7174072fc9b54d22f58ea06e70b73
AndonMitev <mitevandon94@gmail.com>
2021-08-25 23:57:17 +0300

    fix: revert tests from integration

--- tests/integration/03_receive.spec.js
+++ tests/integration/03_receive.spec.js
@@ -35,7 +35,7 @@ async function importWalletTestReceive(bitcoin) {
   expect(yourCurrentAddress).contains(bitcoin)
   await receivePage.HasQRCodeDisplayed(page)
   if (bitcoin === 'ETH' || bitcoin === 'ARBETH' || bitcoin === 'RBTC' ||
-    bitcoin === 'BNB' || bitcoin === 'MATIC' || bitcoin === 'ARBETH' || bitcoin === 'SOL') {
+    bitcoin === 'BNB' || bitcoin === 'MATIC' || bitcoin === 'ARBETH') {
     await receivePage.CheckReceiveURL(page)
   }
   await receivePage.CheckReceiveAddresses(page)
@@ -102,7 +102,7 @@ describe('Liquality wallet- Receive tokens ["mainnet","smoke"]', async () => {
       await overviewPage.CheckAssertOverviewDetails(page, 'BTC')
     })
   })
-  const tokens = ['BTC', 'ETH', 'DAI', 'BNB', 'NEAR', 'ARBETH', 'RBTC', 'SOV', 'MATIC', 'PWETH', 'ARBETH', 'SOL']
+  const tokens = ['BTC', 'ETH', 'DAI', 'BNB', 'NEAR', 'ARBETH', 'RBTC', 'SOV', 'MATIC', 'PWETH', 'ARBETH']
   describe('Import wallet, Receive tokens', async () => {
     beforeEach(async () => {
       browser = await puppeteer.launch(testUtil.getChromeOptions())
