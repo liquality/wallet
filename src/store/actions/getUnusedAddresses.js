7dec88bc2350a2cce62469eb39386bbfaa432afb
AndonMitev <mitevandon94@gmail.com>
2021-08-26 14:03:07 +0300

    fix: Remove space

--- src/store/actions/getUnusedAddresses.js
+++ src/store/actions/getUnusedAddresses.js
@@ -16,7 +16,7 @@ export const getUnusedAddresses = async ({ state, commit, getters }, { network, 
           accountId: account.id
         }
       ).wallet.getUnusedAddress()
-       const address = isEthereumChain(asset) ? result.address.replace('0x', '') : result.address // TODO: Should not require removing 0x
+      const address = isEthereumChain(asset) ? result.address.replace('0x', '') : result.address // TODO: Should not require removing 0x
       let updatedAddresses = []
       if (account.chain === ChainId.Bitcoin) {
         if (!account.addresses.includes(address)) {
