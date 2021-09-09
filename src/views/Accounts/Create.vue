9d5e4c0f6ef96e530841e762cb99c1a03d4a3d54
Bradley <bradleysuira@gmail.com>
2021-09-08 22:27:15 -0500

    feat: update create view

--- src/views/Accounts/Create.vue
+++ src/views/Accounts/Create.vue
@@ -11,30 +11,30 @@
           <div class="create-item-row-title">Select the corresponding Blockchain</div>
           <div class="dropdown" v-click-away="hideAssetList">
           <button class="btn custom-dropdown-toggle" @click="toggleAssetList">
-            <div class="form" v-if="selectedAsset">
+            <div class="form" v-if="selectedChain">
               <div class="input-group">
                 <img
-                  :src="getAssetIcon(selectedAsset.name)"
+                  :src="getAssetIcon(selectedChain.nativeAsset)"
                   class="asset-icon"
                 />
                 <span class="input-group-text">
-                  {{ selectedAsset.label }}
+                  {{ selectedChain.name }}
                 </span>
               </div>
             </div>
             <ChevronRightIcon :class="{ open: assetsDropdownOpen }" />
           </button>
           <ul class="dropdown-menu custom-dropdown-menu" :class="{ show: assetsDropdownOpen }">
-            <li v-for="asset in assetList" :key="asset.name">
-              <a class="dropdown-item" href="#" @click="selectAsset(asset)">
+            <li v-for="chain in chains" :key="chain.code">
+              <a class="dropdown-item" href="#" @click="selectChain(chain)">
                  <div class="form">
               <div class="input-group">
                 <img
-                  :src="getAssetIcon(asset.name)"
+                  :src="getAssetIcon(chain.nativeAsset)"
                   class="asset-icon"
                 />
                 <span class="input-group-text">
-                  {{ asset.label }}
+                  {{ chain.name }}
                 </span>
               </div>
             </div>
@@ -55,7 +55,7 @@
         <button
           class="btn btn-primary btn-lg btn-icon"
           @click="createAccount"
-          :disabled="loading || !selectedAsset"
+          :disabled="loading || !selectedChain"
         >
           <SpinnerIcon class="btn-loading" v-if="loading" />
           <template v-else>Create</template>
@@ -71,7 +71,9 @@ import NavBar from '@/components/NavBar.vue'
 import { getAssetIcon } from '@/utils/asset'
 import ChevronRightIcon from '@/assets/icons/chevron_right_gray.svg'
 import clickAway from '@/directives/clickAway'
-import { mapState } from 'vuex'
+import { mapGetters } from 'vuex'
+import SpinnerIcon from '@/assets/icons/spinner.svg'
+import { chains } from '@liquality/cryptoassets'
 
 export default {
   directives: {
@@ -79,33 +81,33 @@ export default {
   },
   components: {
     NavBar,
-    ChevronRightIcon
+    ChevronRightIcon,
+    SpinnerIcon
   },
   data () {
     return {
       loading: false,
-      selectedAsset: null,
+      selectedChain: null,
       assetsDropdownOpen: false
     }
   },
   computed: {
-    ...mapState(['networkAssets']),
-    assetList () {
-      return this.networkAssets
-        .filter(a => a !== this.selectedAsset)
-        .map()
+    ...mapGetters(['networkAssets']),
+    chains () {
+      return Object.values(chains)
+        .filter(chain => chain.code !== this.selectedChain?.code)
     }
   },
   created () {
-    this.selectedAsset = this.assetList[0]
+    this.selectedChain = Object.values(chains)[0]
   },
   methods: {
     getAssetIcon,
     goToOverview () {
       this.$router.replace('/wallet')
     },
-    selectAsset (asset) {
-      this.selectedAsset = asset
+    selectChain (chain) {
+      this.selectedChain = chain
     },
     toggleAssetList () {
       this.assetsDropdownOpen = !this.assetsDropdownOpen
@@ -115,12 +117,30 @@ export default {
     },
     createAccount () {
       // TODO: implement create account
+    },
+    getNextAccountIndex () {
+      //
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.dropdown {
+  text-transform: none !important;
+  .input-group-text {
+    font-weight: 300 !important;
+    font-size: 12px !important;
+    line-height: 150% !important;
+    margin-left: 5px;
+  }
+}
+
+.custom-dropdown-menu {
+  max-width: 100% !important;
+  max-height: 250px !important;
+}
+
 .wrapper {
   padding-top: 0 !important;
   .create-item-row-title {
@@ -130,11 +150,11 @@ export default {
   .create-item-row {
     padding: 26px 0px;
     font-style: normal;
-      font-weight: bold;
-      font-size: 12px;
-      line-height: 16px;
-      text-transform: uppercase;
-      color: #3D4767;
+    font-weight: bold;
+    font-size: 12px;
+    line-height: 16px;
+    text-transform: uppercase;
+    color: #3D4767;
   }
 }
 </style>
