84b3c2bbdcb6787a6e0e567d1830ec5a83b9a262
Raja Beemi <beemi.raja@gmail.com>
2021-09-17 12:36:49 +0100

    feat: hotjar installation

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import store from './store'
 import router from './router'
 import ToggleButton from 'vue-js-toggle-button'
 import VTooltip from 'v-tooltip'
+import Hotjar from 'vue-hotjar'
 
 import '@/assets/scss/style.scss'
 
+Vue.use (Hotjar, {
+  id: '1260353'
+})
 Vue.use(ToggleButton)
 Vue.use(VTooltip)
 Vue.config.productionTip = false
