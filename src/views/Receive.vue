1665ef5a03f0c812ce56ea4bed7c61734f515dc1
AndonMitev <mitevandon94@gmail.com>
2021-09-22 11:31:18 +0300

    feat: Remove password from unlock page

--- src/views/Receive.vue
+++ src/views/Receive.vue
@@ -118,7 +118,7 @@ export default {
             url: 'https://testnet-faucet.mempool.co/'
           },
           ETH: {
-            name: 'Ethererum ropsten',
+            name: 'Ethererum Ropsten',
             url: 'https://faucet.dimensions.network/'
           },
           RBTC: {
