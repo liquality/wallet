a05032a4f696eeff032f78720e6bee2e860e13f1
b0b <91906195+b0b4456@users.noreply.github.com>
2021-10-11 12:00:56 -0700

    fix dropdown positioning

--- src/components/ChainDropdown.vue
+++ src/components/ChainDropdown.vue
@@ -170,6 +170,8 @@ export default {
     padding-bottom: 0;
     padding-top: 0;
     margin: 0;
+    right: 0;
+    left: auto;
     border: 1px solid #D9DFE5;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
