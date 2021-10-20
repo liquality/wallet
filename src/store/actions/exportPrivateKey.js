a02fb0158aa6ba6b44f7f49f086c8c17827b3643
Bradley <bradleysuira@gmail.com>
2021-10-20 15:33:03 -0500

    feat: access to chain with chainId in exportPrivatekey action

--- src/store/actions/exportPrivateKey.js
+++ src/store/actions/exportPrivateKey.js
@@ -1,12 +1,18 @@
+import { chains } from '@liquality/cryptoassets'
+
 export const exportPrivateKey = async ({ getters }, args) => {
   const { network, walletId, accountId, chainId } = args
   console.log('here', chainId)
 
+  const asset = chains[chainId]?.nativeAsset
+  if (!asset) {
+    // throw error
+  }
   const client = getters.client({
     network,
     walletId,
     accountId,
-    asset: `FIXME-${chainId}`
+    asset
   })
 
   console.log(client)
