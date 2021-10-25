f74bf9c130f69a24136988711ae7e0758102074f
Andon Mitev <32274987+AndonMitev@users.noreply.github.com>
2021-10-25 17:59:00 +0300

    fix: Reduce claim fee for terra (#350)
    * fix: Reduce claim fee
    
    * fix: Fee on initiate
    
    * fix: lint

--- src/swaps/liquality/LiqualitySwapProvider.js
+++ src/swaps/liquality/LiqualitySwapProvider.js
@@ -503,7 +503,7 @@ class LiqualitySwapProvider extends SwapProvider {
       BNB: 165000,
       NEAR: 10000000000000,
       SOL: 2,
-      LUNA: 650000,
+      LUNA: 6500000,
       MATIC: 165000,
       ERC20: 600000 + 94500, // Contract creation + erc20 transfer
       ARBETH: 2400000
@@ -516,7 +516,7 @@ class LiqualitySwapProvider extends SwapProvider {
       MATIC: 45000,
       NEAR: 8000000000000,
       SOL: 1,
-      LUNA: 650000,
+      LUNA: 440000,
       ERC20: 100000,
       ARBETH: 680000
     }
