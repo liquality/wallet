4980a7e44db9c896b8dc72a1fb771faf277fa535
monokh <mnokhb@gmail.com>
2021-10-27 21:26:30 +0100

    fix: sanity checks around enabling terra

--- src/store/migrations/16_enable_terra_chain.js
+++ src/store/migrations/16_enable_terra_chain.js
@@ -13,26 +13,37 @@ export const enableTerraChain = {
       enabledChains[walletId] = {}
 
       for (const network of Networks) {
-        const chain = chains[ChainId.Terra]
-        const derivationPath = getDerivationPath(ChainId.Terra, network, 0, 'default')
-        const terraAccount = accountCreator({
-          walletId,
-          network,
-          account: {
-            name: `${chain.name} 1`,
-            alias: '',
-            chain: ChainId.Terra,
-            addresses: [],
-            assets: ['LUNA', 'UST'],
-            balances: {},
-            type: 'default',
-            index: 0,
-            derivationPath,
-            color: getNextAccountColor(ChainId.Terra, 0)
-          }
-        })
-        accounts[walletId][network] = [...state.accounts[walletId][network], terraAccount]
-        enabledChains[walletId][network] = [...state.enabledChains[walletId][network], ChainId.Terra]
+        const accountExists = state.accounts[walletId][network].find(account => account.chain === ChainId.Terra)
+        if (accountExists) {
+          accounts[walletId][network] = [...state.accounts[walletId][network]]
+        } else {
+          const chain = chains[ChainId.Terra]
+          const derivationPath = getDerivationPath(ChainId.Terra, network, 0, 'default')
+          const terraAccount = accountCreator({
+            walletId,
+            network,
+            account: {
+              name: `${chain.name} 1`,
+              alias: '',
+              chain: ChainId.Terra,
+              addresses: [],
+              assets: ['LUNA', 'UST'],
+              balances: {},
+              type: 'default',
+              index: 0,
+              derivationPath,
+              color: getNextAccountColor(ChainId.Terra, 0)
+            }
+          })
+          accounts[walletId][network] = [...state.accounts[walletId][network], terraAccount]
+        }
+
+        const chainEnabled = state.enabledChains[walletId][network].includes(ChainId.Terra)
+        if (chainEnabled) {
+          enabledChains[walletId][network] = [...state.enabledChains[walletId][network]]
+        } else {
+          enabledChains[walletId][network] = [...state.enabledChains[walletId][network], ChainId.Terra]
+        }
       }
     }
 
@@ -40,7 +51,9 @@ export const enableTerraChain = {
     for (const network of Networks) {
       enabledAssets[network] = {}
       for (const walletId in state.enabledAssets[network]) {
-        enabledAssets[network][walletId] = [...state.enabledAssets[network][walletId], 'LUNA', 'UST']
+        enabledAssets[network][walletId] = [...state.enabledAssets[network][walletId]]
+        if (!enabledAssets[network][walletId].includes('LUNA')) enabledAssets[network][walletId].push('LUNA')
+        if (!enabledAssets[network][walletId].includes('UST')) enabledAssets[network][walletId].push('UST')
       }
     }
 
