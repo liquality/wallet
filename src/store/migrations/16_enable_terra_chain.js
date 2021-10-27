59ba9b73ba5a67211bf50adf3ca9bca25a57187e
monokh <mnokhb@gmail.com>
2021-10-27 11:12:18 +0100

    fix: Enable terra chain for users updating

---
+++ src/store/migrations/16_enable_terra_chain.js
@@ -0,0 +1,46 @@
+import { accountCreator, getNextAccountColor } from '@/utils/accounts'
+import { Networks } from '@/utils/networks'
+import { getDerivationPath } from '@/utils/derivationPath'
+import { chains, ChainId } from '@liquality/cryptoassets'
+
+export const enableTerraChain = {
+  version: 16,
+  migrate: async (state) => {
+    const accounts = {}
+    const enabledChains = {}
+
+    for (const walletId in state.accounts) {
+      accounts[walletId] = {}
+      enabledChains[walletId] = {}
+
+      for (const network of Networks) {
+        const chain = chains[ChainId.Terra]
+        const derivationPath = getDerivationPath(ChainId.Terra, network, 0, 'default')
+        const terraAccount = accountCreator({
+          walletId,
+          network,
+          account: {
+            name: `${chain.name} 1`,
+            alias: '',
+            chain: ChainId.Terra,
+            addresses: [],
+            assets: ['LUNA', 'UST'],
+            balances: {},
+            type: 'default',
+            index: 0,
+            derivationPath,
+            color: getNextAccountColor(ChainId.Terra, 0)
+          }
+        })
+        accounts[walletId][network] = [...state.accounts[walletId][network], terraAccount]
+        enabledChains[walletId][network] = [...state.enabledChains[walletId][network], ChainId.Terra]
+      }
+    }
+
+    return {
+      ...state,
+      enabledChains,
+      accounts
+    }
+  }
+}
