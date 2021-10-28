24109a3ac0ed538f1df0a95119374bac381c8ffd
Andon Mitev <32274987+AndonMitev@users.noreply.github.com>
2021-10-28 13:13:40 +0300

    fix: If one of the request for getting balance failed all of the concurrent requests were not executed (#348)
    * fix: If one of the request for getting balance failed all of the concurent requests were not executed
    
    * fix: Apply pr comments
    
    * feat: Add log for the err
    
    * feat: Use console.error

--- src/store/actions/updateBalances.js
+++ src/store/actions/updateBalances.js
@@ -35,11 +35,15 @@ export const updateBalances = async ({ state, commit, getters }, { network, wall
         addresses = await _client.wallet.getUsedAddresses()
       }
 
-      const balance = addresses.length === 0
-        ? 0
-        : (await _client.chain.getBalance(addresses)).toNumber()
+      try {
+        const balance = addresses.length === 0
+          ? 0
+          : (await _client.chain.getBalance(addresses)).toNumber()
 
-      commit('UPDATE_BALANCE', { network, accountId: account.id, walletId, asset, balance })
+        commit('UPDATE_BALANCE', { network, accountId: account.id, walletId, asset, balance }) 
+      } catch (err) {
+        console.error(err)
+       }
 
       // Commit to the state the addresses
       let updatedAddresses = []
