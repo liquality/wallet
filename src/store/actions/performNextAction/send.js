bf6bc9e1b6a59bf4b4c0a4b2e6672e2f038e83b9
Slav Keremidchiev <skeremidch@gmail.com>
2021-11-16 20:27:01 +0200

    feat: transaction failure if in pending state too long

--- src/store/actions/performNextAction/send.js
+++ src/store/actions/performNextAction/send.js
@@ -1,5 +1,6 @@
 
 import { withInterval } from './utils'
+import { TxStatus } from '@liquality/types'
 
 async function waitForConfirmations ({ getters, dispatch }, { transaction, network, walletId }) {
   const client = getters.client(
@@ -12,6 +13,17 @@ async function waitForConfirmations ({ getters, dispatch }, { transaction, netwo
   )
   try {
     const tx = await client.chain.getTransactionByHash(transaction.txHash)
+
+    // In case transaction doesn't exist: wait for 30 minutes then return failed state
+    // Could happen on Dropped&Replaced
+    if (!tx && Date.now() - transaction.startTime > 1800000) {
+      return {
+        endTime: Date.now(),
+        status: TxStatus.Failed
+      }
+    }
+
+    // In case transaction exists and has more than 0 confirmations: return tx state
     if (tx && tx.confirmations > 0) {
       dispatch('updateBalances', { network, walletId, assets: [transaction.from] })
 
