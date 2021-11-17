12f0c2a65eb3834cfa1e55e80bee4d71867a1c0d
Bradley <bradleysuira@gmail.com>
2021-11-17 14:51:34 -0500

    feat: updated rsk endpoints to use sovryn endpoints

--- src/store/factory/client.js
+++ src/store/factory/client.js
@@ -177,7 +177,7 @@ function createSolanaClient (network, mnemonic, derivationPath) {
 function createRskClient (asset, network, mnemonic, accountType, derivationPath) {
   const isTestnet = network === 'testnet'
   const rskNetwork = ChainNetworks.rsk[network]
-  const rpcApi = isTestnet ? 'https://public-node.testnet.rsk.co' : 'https://public-node.rsk.co'
+  const rpcApi = isTestnet ? 'https://testnet.sovryn.app/rpc' : 'https://mainnet.sovryn.app/rpc'
   const scraperApi = isTestnet ? 'https://liquality.io/rsk-testnet-api' : 'https://liquality.io/rsk-mainnet-api'
   const feeProvider = new EthereumRpcFeeProvider({ slowMultiplier: 1, averageMultiplier: 1, fastMultiplier: 1.25 })
 
