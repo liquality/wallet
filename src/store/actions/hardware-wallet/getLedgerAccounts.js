db42141e90c9a4268fee538773cc24ab0bafba24
Bradley <bradleysuira@gmail.com>
2021-12-01 23:52:47 -0500

    feat: get xPub calling the ledger bridge without CAL

--- src/store/actions/hardware-wallet/getLedgerAccounts.js
+++ src/store/actions/hardware-wallet/getLedgerAccounts.js
@@ -1,5 +1,19 @@
 
-import { assets, chains } from '@liquality/cryptoassets'
+import { assets, chains, ChainId } from '@liquality/cryptoassets'
+import { callToBridge } from '@/utils/ledger-bridge-provider'
+import { getDerivationPath } from '@/utils/derivationPath'
+import {
+  RequestNamespace,
+  ExecutionMode
+} from '@liquality/hw-web-bridge'
+import { findCryptoCurrencyById } from '@ledgerhq/cryptoassets'
+
+const getXPUBVersion = (network) => {
+  const id = network === 'mainnet' ? 'bitcoin' : 'bitcoin_testnet'
+  const { bitcoinLikeInfo: { XPUBVersion } } = findCryptoCurrencyById(id)
+
+  return XPUBVersion
+}
 
 export const getLedgerAccounts = async (
   { getters },
@@ -13,18 +27,36 @@ export const getLedgerAccounts = async (
   })
 
   const pageIndexes = [...Array(numAccounts || 5).keys()].map(i => i + startingIndex)
+  const xpubVersion = getXPUBVersion(network)
+  console.log('xpubVersion', xpubVersion, parseInt(xpubVersion, 10).toString(16))
   for (const index of pageIndexes) {
-    const _client = client(
-      {
+    let addresses = []
+
+    if (chain === ChainId.Bitcoin) {
+      const path = getDerivationPath(chain, network, index, accountType)
+      const xPub = await callToBridge({
+        namespace: RequestNamespace.App,
         network,
-        walletId,
-        asset,
-        accountType,
-        accountIndex: index,
-        useCache: false
-      }
-    )
-    const addresses = await _client.wallet.getAddresses()
+        chainId: chain,
+        action: 'getWalletXpub',
+        execMode: ExecutionMode.Async,
+        payload: [{ path, xpubVersion }]
+      })
+      console.log('xPub', xPub)
+    } else {
+      const _client = client(
+        {
+          network,
+          walletId,
+          asset,
+          accountType,
+          accountIndex: index,
+          useCache: false
+        }
+      )
+      addresses = await _client.wallet.getAddresses()
+    }
+
     if (addresses && addresses.length > 0) {
       const account = addresses[0]
       const normalizedAddress = chains[chain].formatAddress(account.address, network)
