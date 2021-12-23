e033606a7f4c15ef55fd347aa0bd0c6e9c7fb607
Raja Beemi <beemi.raja@gmail.com>
2021-12-23 14:15:34 +0000

    feat: Hot fix 0.52.0 release

--- tests/e2e/17_uniswap_dapp_injection.spec.js
+++ tests/e2e/17_uniswap_dapp_injection.spec.js
@@ -15,7 +15,7 @@ const password = '123123123'
 const dappUrl = 'https://app.uniswap.org/#/swap'
 let ethereumChainId, arbitrumChainId
 
-describe('Uniswap Dapp Injection-[mainnet,testnet,smoke]', async () => {
+describe.skip('Uniswap Dapp Injection-[mainnet,testnet,smoke]', async () => {
   beforeEach(async () => {
     browser = await puppeteer.launch(testUtil.getChromeOptions())
     page = await browser.newPage()
