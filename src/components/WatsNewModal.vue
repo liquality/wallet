4ee703217ef5d9b701707265015c59c51908bb62
Raja Beemi <beemi.raja@gmail.com>
2022-01-26 14:26:21 +0000

    chore: Dev wallet 0.53.16

--- src/components/WatsNewModal.vue
+++ src/components/WatsNewModal.vue
@@ -10,11 +10,11 @@
             <ChartIcon />
           </div>
           <div class="item-content">
-            <pre>Integrated Thorchain for cross-chain swaps</pre>
+            <pre class="text-wrap">Integrated Thorchain for cross-chain swaps</pre>
             <pre>Upgraded EIP-1559 compatibility</pre>
             <pre>Updated 1inch integration to v4</pre>
             <pre>Improved dapp injection UI/UX</pre>
-            <pre>Detailed "send" transaction timeline</pre>
+            <pre class="text-wrap">Detailed "send" transaction timeline</pre>
             <pre>Faster loading/start screens</pre>
             <pre class="text-wrap">Fewer node errors</pre>
             <pre class="text-wrap">Minor bug fixes</pre>
