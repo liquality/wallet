949ef9ef5c0b38866579fa229dee872dec6f735b
Raj Beemi <beemi.raja@gmail.com>
2022-02-14 17:22:25 +0000

    fix: remove update balance

--- src/background.js
+++ src/background.js
@@ -206,17 +206,5 @@ store.subscribe(async ({ type, payload }, state) => {
         }
       })
       break
-    case 'UPDATE_BALANCE':
-      // if (payload.balance > 0) {
-      //   dispatch('trackAnalytics', {
-      //     event: 'Hold Asset',
-      //     properties: {
-      //       category: 'Hold Asset',
-      //       action: 'Hold asset greater than 0',
-      //       asset: `${payload.asset}`
-      //     }
-      //   })
-      // }
-      break
   }
 })
