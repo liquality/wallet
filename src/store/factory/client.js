98062c0593feb58eefdfc0f855270dcbe15d3c7b
AndonMitev <mitevandon94@gmail.com>
2022-03-02 15:18:18 +0200

    feat: Update terra node

--- src/store/factory/client.js
+++ src/store/factory/client.js
@@ -326,9 +326,13 @@ function createArbitrumClient(asset, network, mnemonic, derivationPath) {
 }
 
 function createTerraClient(network, mnemonic, baseDerivationPath, asset) {
+  const isTestnet = network === 'testnet'
+  const terraNetwork = ChainNetworks.terra[network]
+
   let _asset, feeAsset, tokenAddress, stableFee
 
-  const terraNetwork = ChainNetworks.terra[network]
+  const nodeUrl = isTestnet ? terraNetwork.nodeUrl : process.env.VUE_APP_TERRA_NODE_URL
+
 
   switch (asset) {
     case 'LUNA': {
@@ -350,12 +354,14 @@ function createTerraClient(network, mnemonic, baseDerivationPath, asset) {
     }
   }
 
+  console.log(network, nodeUrl)
+
   const terraClient = new Client()
 
-  terraClient.addProvider(new TerraRpcProvider(terraNetwork, _asset, feeAsset, tokenAddress))
+  terraClient.addProvider(new TerraRpcProvider({ ...terraNetwork, nodeUrl }, _asset, feeAsset, tokenAddress))
   terraClient.addProvider(
     new TerraWalletProvider({
-      network: terraNetwork,
+      network: { ...terraNetwork, nodeUrl },
       mnemonic,
       baseDerivationPath,
       asset: _asset,
@@ -364,8 +370,8 @@ function createTerraClient(network, mnemonic, baseDerivationPath, asset) {
       stableFee
     })
   )
-  terraClient.addProvider(new TerraSwapProvider(terraNetwork, _asset))
-  terraClient.addProvider(new TerraSwapFindProvider(terraNetwork, _asset))
+  terraClient.addProvider(new TerraSwapProvider({ ...terraNetwork, nodeUrl }, _asset))
+  terraClient.addProvider(new TerraSwapFindProvider({ ...terraNetwork, nodeUrl }, _asset))
 
   return terraClient
 }
