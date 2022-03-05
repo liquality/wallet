fb920cd7d793143cf4a9b8b8057627aac787f8b8
Raj Beemi <beemi.raja@gmail.com>
2022-03-05 01:29:23 +0000

    feat: 0.57.0 release candidate cut

--- src/components/WatsNewModal.vue
+++ src/components/WatsNewModal.vue
@@ -10,9 +10,7 @@
             <ChartIcon />
           </div>
           <div class="item-content">
-            <pre class="text-wrap">Astroport integration: swap all Terra tokens with one click</pre>
-            <pre class="text-wrap">Atomic swap UST, LUNA cross chain</pre>
-            <pre class="text-wrap">Minor bug fixes</pre>
+            <pre class="text-wrap">Next release candidate 0.57.0</pre>
           </div>
         </div>
       </div>
