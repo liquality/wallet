564df27a41b35f16f37765705257868748040e52
Raj Beemi <beemi.raja@gmail.com>
2022-03-22 15:16:25 +0000

    chore: Dev release 0.58.1

--- src/components/WatsNewModal.vue
+++ src/components/WatsNewModal.vue
@@ -10,9 +10,7 @@
             <ChartIcon />
           </div>
           <div class="item-content">
-            <pre class="text-wrap">Avalanche network Integration</pre>
-            <pre class="text-wrap">Avalanche swaps with 1Inch</pre>
-            <pre class="text-wrap">Avalanche swaps enabled with Boost</pre>
+            <pre class="text-wrap">THIS IS FOR 0.58.0 RELEASE BUILD</pre>
           </div>
         </div>
       </div>
