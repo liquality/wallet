54a662744e01417418102299c7fdabaaa9509229
monokh <mnokhb@gmail.com>
2022-04-07 21:26:17 +0100

    fix: manage accounts - account icon

--- src/views/Accounts/ChainAccounts.vue
+++ src/views/Accounts/ChainAccounts.vue
@@ -66,7 +66,7 @@ import { mapState, mapGetters, mapActions } from 'vuex'
 import cryptoassets from '@liquality/wallet-core/dist/utils/cryptoassets'
 import PlusIcon from '@/assets/icons/plus_circle.svg'
 import { formatFiat, formatFiatUI } from '@liquality/wallet-core/dist/utils/coinFormatter'
-import { getAccountIcon } from '@liquality/wallet-core/dist/utils/accounts'
+import { getAccountIcon } from '@/utils/accounts'
 import { getChainIcon } from '@/utils/accounts'
 import ListItem from '@/components/ListItem'
 
