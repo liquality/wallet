f05c097549ca21b5a895248216620e7d6a811656
beemi <beemi.raja@gmail.com>
2022-04-21 12:25:00 +0100

    test: Thorechain BTC to ETH test to asset check added

--- test/e2e/service_provider_tests/thorechain_swaps.spec.js
+++ test/e2e/service_provider_tests/thorechain_swaps.spec.js
@@ -4,9 +4,6 @@ const HomePage = require('../../pages/HomePage')
 const PasswordPage = require('../../pages/PasswordPage')
 const SwapPage = require('../../pages/SwapPage')
 const expect = require('chai').expect
-
-const https = require('https')
-
 const puppeteer = require('puppeteer')
 
 const testUtil = new TestUtil()
@@ -44,6 +41,16 @@ describe('ThoreChain SWAP provider["MAINNET", "PULL_REQUEST_TEST"]', async () =>
     await page.waitForSelector('#BTC_swap_button', { visible: true })
     await page.click('#BTC_swap_button')
     console.log('User clicked on BTC SWAP button')
+    try {
+      await page.waitForSelector('#ETH_swap_receive_pair_asset', { visible: true, timeout: 10000 })
+    } catch (e) {
+      if (e instanceof puppeteer.errors.TimeoutError) {
+        expect(
+          e,
+          'BTC>ETH swap ETH should be automatically selected as to assert, something is wrong'
+        ).to.be.null
+      }
+    }
     const swapSendAmountField = await swapPage.GetSwapSendAmount(page)
     expect(swapSendAmountField, 'BTC to ETH SWAP min value not set in input').not.equals('0.0000')
     await swapPage.ClickOnMin(page)
