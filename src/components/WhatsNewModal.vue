9fbd53424ab500763a23e925dfe25975a596f496
Raj Beemi <beemi.raja@gmail.com>
2022-05-31 21:52:56 +0100

    fix: copy update

--- src/components/WhatsNewModal.vue
+++ src/components/WhatsNewModal.vue
@@ -10,7 +10,9 @@
             <ChartIcon />
           </div>
           <div class="item-content">
-            <pre class="text-wrap">* Buy crypto here with your debit/credit Card or Bank Transfer via Transak!</pre>
+            <pre class="text-wrap">
+* Buy crypto here with your debit/credit Card or Bank Transfer via Transak!</pre
+            >
             <pre class="text-wrap">
 * Access OpenSea's marketplace by setting Liquality as your Default Web3 Wallet in Settings</pre
             >
