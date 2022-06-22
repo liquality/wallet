544d35b0a5da951188b1ea0244be97f36692c375
tima-t <tito.marchev@gmail.com>
2022-06-23 00:37:43 +0300

    fix: add more accurate check for is pair tradable

--- src/views/Swap/Swap.vue
+++ src/views/Swap/Swap.vue
@@ -763,9 +763,8 @@ export default {
     isPairAvailable() {
       const liqualityMarket = this.networkMarketData?.find(
         (pair) =>
-          pair.from === this.asset &&
-          (pair.to === this.toAsset ||
-            buildConfig.supportedBridgeAssets.indexOf(this.toAssetChain) !== -1)
+          (pair.from === this.asset || (buildConfig.supportedBridgeAssets.indexOf(this.assetChain) !== -1)) &&
+          (pair.to === this.toAsset || (buildConfig.supportedBridgeAssets.indexOf(this.toAssetChain) !== -1))
       )
       return !!liqualityMarket
     },
