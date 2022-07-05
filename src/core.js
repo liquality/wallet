00ffcbef2d0d30dd262ed76d4333cd61b6080b1b
Bradley <bradleysuira@gmail.com>
2022-07-05 05:07:24 -0500

    fix: convert big number values to send the amount in query string

--- src/core.js
+++ src/core.js
@@ -2,7 +2,7 @@ import { setupWallet } from '@liquality/wallet-core'
 import { AES } from 'crypto-js'
 import _pbkdf2 from 'pbkdf2'
 import { createNotification as _createNotification } from './utils/notification'
-import { WebHidTransportCreator } from '@chainify/hw-ledger';
+import { WebHidTransportCreator } from '@chainify/hw-ledger'
 
 async function pbkdf2(password, salt, iterations, length, digest) {
   return new Promise((resolve, reject) => {
