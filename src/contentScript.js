05d74186193120fb33e76bc68e31f1cd55725966
monokh <mnokhb@gmail.com>
2022-09-09 10:05:25 +0100

    fix: remove unused import

--- src/contentScript.js
+++ src/contentScript.js
@@ -2,7 +2,7 @@ import { inject } from './broker/utils'
 import Script from './broker/Script'
 import { buildConfig } from '@liquality/wallet-core'
 import { ChainNetworks } from '@liquality/wallet-core/dist/src/utils/networks'
-import { chains, getNativeAssetCode, isEvmChain } from '@liquality/cryptoassets'
+import { getNativeAssetCode, isEvmChain } from '@liquality/cryptoassets'
 import PortStream from 'extension-port-stream'
 import LocalMessageDuplexStream from 'post-message-stream'
 new Script().start()
