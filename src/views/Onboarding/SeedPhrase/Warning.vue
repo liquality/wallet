0b3d9bc3d7f11dd906a8c2fef34daadca8c8c4bc
Jennie <echenimjennifer@gmail.com>
2022-09-24 04:24:29 +0400

    fix: backup seed page - style modifications

--- src/views/Onboarding/SeedPhrase/Warning.vue
+++ src/views/Onboarding/SeedPhrase/Warning.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="wallet warning-phrase">
+  <div class="walletwarning-phrase">
     <NavBar showMenu="true">
       <strong>Warning</strong>
     </NavBar>
@@ -14,20 +14,22 @@
     <div class="mx-auto text-center">
       <h5 class="mx-auto px-5">View it in private without cameras around.</h5>
     </div>
-    <div class="wrapper_bottom">
-      <div class="button-group">
-        <button
-          @click="$router.back()"
-          class="btn btn-outline-primary btn-lg width-button"
-          id="show_seed_phrase_cancel_button"
-        >
-          Cancel
-        </button>
-        <router-link :to="nextPath"
-          ><button class="btn btn-primary btn-lg width-button" id="i_have_privacy_button">
-            I have privacy
-          </button></router-link
-        >
+    <div class="wrapper">
+      <div class="wrapper_bottom">
+        <div class="button-group">
+          <button
+            @click="$router.back()"
+            class="btn btn-light btn-outline-primary btn-lg"
+            id="show_seed_phrase_cancel_button"
+          >
+            Cancel
+          </button>
+          <router-link :to="nextPath">
+            <button class="btn btn-primary btn-lg" id="i_have_privacy_button">
+              I have privacy
+            </button>
+          </router-link>
+        </div>
       </div>
     </div>
   </div>
@@ -64,6 +66,9 @@ export default {
   &_eye-svg {
     height: 150px;
     width: 150px;
+  }
+  .wrapper {
+    padding: 0 20px;
   }
 }
 </style>
