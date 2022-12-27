f5b483dc7a3928f75752d0ad30a28934e3bd8f71
Bradley Suira <bradleysuira@gmail.com>
2022-12-26 20:34:14 -0500

    fix: removed explorer url because can't be implemented at this time

--- src/components/CustomRpcSettingsForm.vue
+++ src/components/CustomRpcSettingsForm.vue
@@ -29,7 +29,7 @@
     <!-- <div class="rpc-form-control">
       <label>{{ $t(`pages.settings.customRpcForm.blockExplorerUrl`) }}</label>
       <input type="text" v-model="formData.explorerUrl" placeholder="block explorer url" />
-    </div>
+    </div> -->
     <div class="rpc-form-control mt-3">
       <div class="button-group">
         <button
@@ -43,7 +43,7 @@
           {{ $t(`pages.settings.customRpcForm.default`) }}
         </button>
       </div>
-    </div> -->
+    </div>
   </div>
 </template>
 <script>
