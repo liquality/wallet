8a8ffe52e13a726b5e10be9d90954a15ad319597
Mpwanyi Samuel <samuelmpwanyi@gmail.com>
2023-01-06 17:46:17 +0300

    Clean up

--- src/views/Send/SendNFT.vue
+++ src/views/Send/SendNFT.vue
@@ -8,7 +8,7 @@
         <div>
           <Accordion v-for="(assets, key) in nftCollection" :key="assets.id">
             <h3 slot="header" id="nft-asset-header">
-              bohohohho {{ nftCollectionName(assets, key) }} ({{ assets.length }})
+              {{ nftCollectionName(assets, key) }} ({{ assets.length }})
             </h3>
             <div class="nft-assets__container__images">
               <div
