80814762d4b9a95ce7ef3fe0a68b8dfd209da2a2
Bradley Suira <bradleysuira@gmail.com>
2023-01-25 20:20:19 -0500

    fix: fixed permission sign screen for eip 712 and v1

--- src/views/PermissionSign.vue
+++ src/views/PermissionSign.vue
@@ -5,6 +5,53 @@
     </div>
     <div class="permission-sign wrapper text-center">
       <LedgerSignRequestModal :open="signRequestModalOpen" @close="closeSignRequestModal" />
+      <Modal v-if="showErrorModal" :show-close="false">
+        <template>
+          <h5 class="modal-title text-danger">
+            {{ $t('common.warning') }}
+          </h5>
+          <div class="row" v-if="error">
+            <div class="col">
+              {{ error }}
+            </div>
+          </div>
+          <template v-else>
+            <div class="row">
+              <div class="col">
+                {{
+                  $t('pages.permission.chainIdErrorNetwork', {
+                    network: requestedChain ? requestedChain.name : 'UNKNOWN',
+                    origin: request.origin
+                  })
+                }}
+              </div>
+            </div>
+            <div class="row mt-3">
+              <div class="col">
+                <p>
+                  {{
+                    $t('pages.permission.chainIdErrorRequestedChainId', {
+                      chainId: requestedChainId
+                    })
+                  }}
+                </p>
+                <p>
+                  {{
+                    $t('pages.permission.chainIdErrorRequestedAsset', {
+                      asset
+                    })
+                  }}
+                </p>
+              </div>
+            </div>
+          </template>
+        </template>
+        <template #footer>
+          <button class="btn btn-light btn-outline-primary btn-lg" @click="closeErrorModal">
+            {{ $t('common.ok') }}
+          </button>
+        </template>
+      </Modal>
       <div class="wrapper_top form">
         <h2>{{ $t('pages.permission.requestToSign') }}</h2>
         <img :src="getAssetIcon(asset)" class="permission-sign_icon mt-4 mb-2" />
@@ -32,7 +79,7 @@
           <button
             class="btn btn-primary btn-lg btn-icon"
             @click.stop="reply(true)"
-            :disabled="loading"
+            :disabled="loading || error"
           >
             <SpinnerIcon class="btn-loading" v-if="loading" />
             <template v-else>{{ $t('common.sign') }}</template>
@@ -52,11 +99,9 @@ import LogoWallet from '@/assets/icons/logo_wallet.svg?inline'
 import SpinnerIcon from '@/assets/icons/spinner.svg'
 import { isAddress } from 'ethers/lib/utils'
 import LedgerSignRequestModal from '@/components/LedgerSignRequestModal'
+import Modal from '@/components/Modal'
 import { ledgerConnectMixin } from '@/utils/hardware-wallet'
-import {
-  createInternalError,
-  CUSTOM_ERRORS
-} from '@liquality/error-parser/dist/src/LiqualityErrors'
+import { getChain, getAllSupportedChains } from '@liquality/cryptoassets'
 
 const signTypedDataMethodToVersion = {
   eth_signTypedData: 'V1',
@@ -83,6 +128,7 @@ function hexToAscii(hex) {
 export default {
   components: {
     SpinnerIcon,
+    Modal,
     LedgerSignRequestModal
   },
   mixins: [ledgerConnectMixin],
@@ -90,9 +136,13 @@ export default {
     return {
       loading: false,
       replied: false,
+      error: null,
       messageToDisplay: '',
       messageToSign: '',
-      signRequestModalOpen: false
+      signRequestModalOpen: false,
+      showErrorModal: false,
+      requestedChainId: null,
+      requestedChain: null
     }
   },
   methods: {
@@ -128,10 +178,13 @@ export default {
         this.signRequestModalOpen = false
         this.loading = false
       }
+    },
+    closeErrorModal() {
+      this.showErrorModal = false
     }
   },
   computed: {
-    ...mapState(['activeNetwork', 'activeWalletId']),
+    ...mapState(['activeNetwork', 'activeWalletId', 'externalConnections']),
     ...mapGetters(['accountItem']),
     logo() {
       return LogoWallet
@@ -140,7 +193,7 @@ export default {
       return this.request.asset
     },
     address() {
-      return this.request.args[1] || this.request.args[0].params[0]
+      return this.messageToSign?.from || ''
     },
     request() {
       return {
@@ -150,15 +203,22 @@ export default {
     },
     account() {
       return this.accountItem(this.request?.accountId)
+    },
+    dappConnections() {
+      return this.externalConnections[this.activeWalletId] || {}
     }
   },
   created() {
     if (this.request.method === 'wallet.signTypedData') {
       const { method, params } = this.request.args[0]
-
       const first = params[0]
       const second = params[1]
-      const msgParams = { version: signTypedDataMethodToVersion[method] }
+      const msgParams = {
+        version: signTypedDataMethodToVersion[method],
+        from: null,
+        data: {}
+      }
+
       const extra = params[2] || {}
       try {
         if (isAddress(first)) {
@@ -169,11 +229,39 @@ export default {
           msgParams.data = JSON.parse(first)
         }
       } catch (err) {
-        throw createInternalError(CUSTOM_ERRORS.Invalid.Json(err)) // Error(`Invalid JSON: ${err}`)
+        this.error = `Invalid JSON: ${err}`
+        console.error(err)
       }
-
       this.messageToDisplay = Object.entries(msgParams.data)
       this.messageToSign = { ...msgParams, ...extra }
+
+      /**
+       ***** Validations for EIP-712
+       **/
+      this.requestedChainId = `${msgParams.data?.domain?.chainId}`.trim()
+      // the requested chainId can be an integer, string or hex so we should convert it to int if we get an hex
+      const chainIdToValidate = isHex(this.requestedChainId)
+        ? parseInt(this.requestedChainId, 16)
+        : this.requestedChainId
+
+      // We shuold try to get the chain details from the requested chainId if it's valid
+      const supportedChains = getAllSupportedChains()[this.activeNetwork] || {}
+      console.log(supportedChains)
+      this.requestedChain = Object.keys(supportedChains).find(
+        (chain) => supportedChains[chain]?.network?.chainId == chainIdToValidate
+      )
+
+      // We get the chains inside the dapp permissions, the chain name should be inside the list
+      const chains = Object.keys(this.dappConnections[this.request.origin] || {})
+        .map((chainName) => getChain(this.activeNetwork, chainName))
+        .filter((i) => i)
+      if (chains.findIndex((chain) => chain.network.chainId == chainIdToValidate) < 0) {
+        console.error(
+          `The request chainId is not matching with the asset chainId and stored permissions, RequestChainId: ${msgParams.data?.domain?.chainId}`,
+          this.request
+        )
+        this.showErrorModal = true
+      }
     }
     // Handle wallet.signMessage
     else {
